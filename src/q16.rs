03110eef9fa624d1b6aea02a31dc7da53101cefd
Blake Winton <bwinton@latte.ca>
2017-11-02 15:22:37 -0400

    Finish day 16!

--- src/q16.rs
+++ src/q16.rs
@@ -2,9 +2,51 @@
 // Setup.
 
 use day;
+use std::iter::FromIterator;
 
-static INPUT : &'static str = "";
-// static INPUT : &'static str = "";
+// static INPUT : &'static str = "10000";
+// static LENGTH_A : usize = 20;
+static INPUT : &'static str = "10111100110001111";
+static LENGTH_A : usize = 272;
+static LENGTH_B : usize = 35651584;
+
+fn get_checksum(input: &String) -> String {
+  let mut rv = Vec::from_iter(input.chars());
+  while rv.len() % 2 == 0 {
+    let mut new = Vec::new();
+    for pair in rv.chunks(2) {
+      if pair[0] == pair[1] {
+        new.push('1');
+      } else {
+        new.push('0');
+      }
+    }
+    rv = new;
+  }
+  return rv.into_iter().collect();
+}
+
+fn dragon(input: &String) -> String {
+  let mut rv = String::from("0");
+  let temp = input.chars().rev().map(|c| {
+    match c {
+      '0' => '1',
+      _ => '0'
+    }
+  }).collect::<String>();
+  rv.push_str(&temp);
+  return rv;
+}
+
+fn get_result(input: &str, length: usize) -> String {
+  let mut data = String::from(input);
+  while data.len() < length {
+    let extra = dragon(&data);
+    data.push_str(&extra);
+  }
+  data = data[0..length].to_string();
+  return get_checksum(&data);
+}
 
 //-----------------------------------------------------
 // Questions.
@@ -18,13 +60,11 @@ impl day::Day for Q {
 
   fn a(&self) {
     print!("{}A: ", self.number());
-    let result = 0;
-    println!("Result = {}", result);
+    println!("Result = {}", get_result(INPUT, LENGTH_A));
   }
 
   fn b(&self) {
     print!("{}B: ", self.number());
-    let result = 0;
-    println!("Result = {}", result);
+    println!("Result = {}", get_result(INPUT, LENGTH_B));
   }
 }
