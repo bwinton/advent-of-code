b74d5e613f8924209b6d1a444d26e73ae393dccd
Blake Winton <bwinton@latte.ca>
2017-11-02 17:09:41 -0400

    Start day 18.

---
+++ src/q18.rs
@@ -0,0 +1,30 @@
+//-----------------------------------------------------
+// Setup.
+
+use day;
+
+static INPUT : &'static str = "";
+// static INPUT : &'static str = "";
+
+//-----------------------------------------------------
+// Questions.
+
+pub struct Q;
+
+impl day::Day for Q {
+  fn number(&self) -> String {
+    return String::from("18");
+  }
+
+  fn a(&self) {
+    print!("{}A: ", self.number());
+    let result = 0;
+    println!("Result = {}", result);
+  }
+
+  fn b(&self) {
+    print!("{}B: ", self.number());
+    let result = 0;
+    println!("Result = {}", result);
+  }
+}
