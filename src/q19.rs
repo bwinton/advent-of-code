3129681233491aca70130b38a00a9d5b7cb07dbe
Blake Winton <bwinton@latte.ca>
2017-11-02 22:16:45 -0400

    Faster way of doing it.

--- src/q19.rs
+++ src/q19.rs
@@ -2,6 +2,7 @@
 // Setup.
 
 use day;
+use std::collections::VecDeque;
 
 // static INPUT : usize = 5;
 static INPUT : usize = 3014603;
@@ -12,32 +13,22 @@ struct Elf {
   presents: usize
 }
 
-fn run_turn(elves: &mut Vec<Elf>) {
-  // println!("");
-  // println!("start {:?}", elves);
-  for i in 0..elves.len() {
-    let next = (i + 1) % elves.len();
-    if elves[i].presents > 0 {
-      elves[i].presents += elves[next].presents;
-      elves[next].presents = 0;
-    }
-  }
-  // println!("stealing {:?}", elves);
-  elves.retain(|elf| elf.presents > 0);
-  // println!("retain {:?}", elves);
-  // println!("");
-}
-
-fn get_result() -> usize {
-  let mut elves = Vec::new();
+fn get_result_a() -> usize {
+  let mut elves = VecDeque::new();
   for i in 0..INPUT {
-    elves.push(Elf{position: i + 1, presents: 1});
+    elves.push_back(Elf{position: i + 1, presents: 1});
   }
   // println!("{:?}", elves);
   while elves.len() > 1 {
-    run_turn(&mut elves);
+    let front = elves.pop_front().unwrap();
+    let next = elves.pop_front().unwrap();
+    elves.push_back(Elf{position: front.position, presents: front.presents + next.presents});
   }
   elves[0].position
+}
+
+fn get_result_b() -> usize {
+  return 0;
 }
 
 //-----------------------------------------------------
@@ -52,12 +43,11 @@ impl day::Day for Q {
 
   fn a(&self) {
     print!("{}A: ", self.number());
-    println!("Result = {}", get_result());
+    println!("Result = {}", get_result_a());
   }
 
   fn b(&self) {
     print!("{}B: ", self.number());
-    let result = 0;
-    println!("Result = {}", result);
+    println!("Result = {}", get_result_b());
   }
 }
