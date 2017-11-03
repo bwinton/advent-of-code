46d7107e6e9c43b85180174e27b6a9289bf962d6
Blake Winton <bwinton@latte.ca>
2017-11-03 09:57:53 -0400

    Start day 20.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod day;
 
 mod q01; mod q02; mod q03; mod q04; mod q05; mod q06; mod q07; mod q08; mod q09; mod q10;
-mod q11; mod q12; mod q13; mod q14; mod q15; mod q16; mod q17; mod q18; mod q19; // mod q20;
+mod q11; mod q12; mod q13; mod q14; mod q15; mod q16; mod q17; mod q18; mod q19; mod q20;
 // mod q21; mod q22; mod q23; mod q24; mod q25;
 
 use std::env;
@@ -45,7 +45,7 @@ fn main() {
 
   let days = q_vec!(
     q01, q02, q03, q04, q05, q06, q07, q08, q09, q10,
-    q11, q12, q13, q14, q15, q16, q17, q18, q19//, q20
+    q11, q12, q13, q14, q15, q16, q17, q18, q19, q20//,
     // q21, q22, q23, q24, q25
     );
 
