2949a6352099025166283ac830b5bcff420b2ba5
Blake Winton <bwinton@latte.ca>
2017-11-29 14:02:51 -0500

    Finish day 4.

--- src/q04.rs
+++ src/q04.rs
@@ -3,15 +3,37 @@
 
 use day;
 
-static TEST_INPUT : &'static str = "";
-static INPUT : &'static str = "";
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+use std::i32::MAX;
 
-fn process_data_a(_data: &str) -> i32 {
-  0
-}
+static TEST_INPUT : &'static str = "abcdef";
+// static TEST_INPUT : &'static str = "pqrstuv";
+static INPUT : &'static str = "bgvyzdsv";
 
-fn process_data_b(_data: &str) -> i32 {
-  0
+fn process_data(data: &str, zeroes: usize) -> i32 {
+  let mut hasher = Md5::new();
+  let input = data.as_bytes();
+
+  for i in 0..MAX {
+    hasher.input(input);
+    hasher.input(i.to_string().as_bytes());
+
+    let mut output = [0; 16]; // An MD5 is 16 bytes
+    hasher.result(&mut output);
+    let mut first = 0;
+    for value in output.iter().take(zeroes/2) {
+      first += i32::from(*value);
+    }
+    if zeroes % 2 == 1 {
+      first += i32::from(output[zeroes/2] >> 4);
+    }
+    if first == 0 {
+      return i
+    }
+    hasher.reset();
+  }
+  -1
 }
 
 //-----------------------------------------------------
@@ -27,9 +49,9 @@ impl day::Day for Q {
   fn a(&self, use_test_data: bool) {
     print!("{}A: ", self.number());
     let result = if use_test_data {
-      process_data_a(TEST_INPUT)
+      process_data(TEST_INPUT, 5)
     } else {
-      process_data_a(INPUT)
+      process_data(INPUT, 5)
     };
     println!("Result = {}", result);
   }
@@ -37,9 +59,9 @@ impl day::Day for Q {
   fn b(&self, use_test_data: bool) {
     print!("{}B: ", self.number());
     let result = if use_test_data {
-      process_data_b(TEST_INPUT)
+      process_data(TEST_INPUT, 6)
     } else {
-      process_data_b(INPUT)
+      process_data(INPUT, 6)
     };
     println!("Result = {}", result);
   }
