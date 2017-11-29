7e888caaf7a77c0c70143951d022e58029d71595
Blake Winton <bwinton@latte.ca>
2017-11-29 15:43:14 -0500

    Add day 5.

---
+++ src/q05.rs
@@ -0,0 +1,50 @@
+//-----------------------------------------------------
+// Setup.
+
+use day;
+
+static TEST_INPUT : &'static str = "ugknbfddgicrmopn
+aaa
+jchzalrnumimnmhp
+haegwjzuvuyypxyu
+dvszwmarrgswjxmb";
+static INPUT : &'static str = "";
+
+fn process_data_a(_data: &str) -> i32 {
+  0
+}
+
+fn process_data_b(_data: &str) -> i32 {
+  0
+}
+
+//-----------------------------------------------------
+// Questions.
+
+pub struct Q;
+
+impl day::Day for Q {
+  fn number(&self) -> String {
+    String::from("5")
+  }
+
+  fn a(&self, use_test_data: bool) {
+    print!("{}A: ", self.number());
+    let result = if use_test_data {
+      process_data_a(TEST_INPUT)
+    } else {
+      process_data_a(INPUT)
+    };
+    println!("Result = {}", result);
+  }
+
+  fn b(&self, use_test_data: bool) {
+    print!("{}B: ", self.number());
+    let result = if use_test_data {
+      process_data_b(TEST_INPUT)
+    } else {
+      process_data_b(INPUT)
+    };
+    println!("Result = {}", result);
+  }
+}
