d01d065358d544bc6647508e8b29be2516e1e147
Blake Winton <bwinton@latte.ca>
2017-12-02 21:26:01 -0500

    WIP day 9.

---
+++ src/q09.rs
@@ -0,0 +1,81 @@
+//-----------------------------------------------------
+// Setup.
+
+use day;
+
+static INPUT : &'static str = "Faerun to Tristram = 65
+Faerun to Tambi = 129
+Faerun to Norrath = 144
+Faerun to Snowdin = 71
+Faerun to Straylight = 137
+Faerun to AlphaCentauri = 3
+Faerun to Arbre = 149
+Tristram to Tambi = 63
+Tristram to Norrath = 4
+Tristram to Snowdin = 105
+Tristram to Straylight = 125
+Tristram to AlphaCentauri = 55
+Tristram to Arbre = 14
+Tambi to Norrath = 68
+Tambi to Snowdin = 52
+Tambi to Straylight = 65
+Tambi to AlphaCentauri = 22
+Tambi to Arbre = 143
+Norrath to Snowdin = 8
+Norrath to Straylight = 23
+Norrath to AlphaCentauri = 136
+Norrath to Arbre = 115
+Snowdin to Straylight = 101
+Snowdin to AlphaCentauri = 84
+Snowdin to Arbre = 96
+Straylight to AlphaCentauri = 107
+Straylight to Arbre = 14
+AlphaCentauri to Arbre = 46";
+
+fn process_data_a(data: &str) -> i32 {
+  for line in data.lines() {
+    lazy_static! {
+      static ref RE: Regex = Regex::new("^([a-z]+) to ([a-z]+) = ([0-9]+)$").unwrap();
+    }
+  }
+  0
+}
+
+fn process_data_b(_data: &str) -> i32 {
+  0
+}
+
+//-----------------------------------------------------
+// Questions.
+
+pub struct Q;
+
+impl day::Day for Q {
+  fn number(&self) -> String {
+    String::from("9")
+  }
+
+  fn a(&self) {
+    print!("{}A: ", self.number());
+    let result = process_data_a(INPUT);
+    println!("Result = {}", result);
+  }
+
+  fn b(&self) {
+    print!("{}B: ", self.number());
+    let result = process_data_b(INPUT);
+    println!("Result = {}", result);
+  }
+}
+
+#[test]
+fn a() {
+  assert_eq!(process_data_a("London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141"), 605);
+}
+
+#[test]
+fn b() {
+  assert_eq!(process_data_b(""), 0);
+}
