6f41f4aacc200b07b92b84d902f1af1b2b98176c
Blake Winton <bwinton@latte.ca>
2017-12-08 09:53:02 -0500

    Finish day 8!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use] mod macros;
 mod day;
 
-mod q01; mod q02; mod q03; mod q04; mod q05; mod q06; mod q07;// mod q08; mod q09; mod q10;
+mod q01; mod q02; mod q03; mod q04; mod q05; mod q06; mod q07; mod q08;// mod q09; mod q10;
 // mod q11; mod q12; mod q13; mod q14; mod q15; mod q16; mod q17; mod q18; mod q19; mod q20;
 // mod q21; mod q22; mod q23; mod q24; mod q25;
 
@@ -11,6 +11,9 @@ use clap::Arg;
 #[macro_use] extern crate clap;
 // #[macro_use] extern crate itertools;
 #[macro_use] extern crate lazy_static;
+
+#[cfg(test)]
+#[macro_use] extern crate maplit;
 
 extern crate crypto;
 extern crate regex;
@@ -43,7 +46,7 @@ fn main() {
   let args: Vec<&str> = matches.values_of("day").unwrap().collect();
 
   let days = q_vec!(
-    q01, q02, q03, q04, q05, q06, q07//, q08, q09, q10,
+    q01, q02, q03, q04, q05, q06, q07, q08//, q09, q10,
     // q11, q12, q13, q14, q15, q16, q17, q18, q19, q20,
     // q21, q22, q23, q24, q25
     );
