536ab3a001ec3b9e574145a519025e82d10966ed
Blake Winton <bwinton@latte.ca>
2017-12-24 23:25:34 -0500

    Day 22 part a.

--- src/q22.rs
+++ src/q22.rs
@@ -3,39 +3,86 @@
 
 use day;
 
-static INPUT : &'static str = "";
+use std::collections::HashSet;
 
-// enum Direction {
-//   Left,
-//   Up,
-//   Right,
-//   Down
-// }
+static INPUT : &'static str = "....##.#.#.#...#.##.##.#.
+##.####..###..#.#.#.###.#
+.#.#...#.##....#......###
+...#.....##.###....##.###
+#.########.#.#####..##.#.
+.#..#..#.#..#....##.#...#
+.....#.##..#.#.....##..##
+....###....###....###.#..
+..#..#..#..#.##.#.#..##.#
+.##......#...##.#.#.##.#.
+.#####.#.#.##...###...#..
+#..###..#....#....##..#..
+###..#....#.##.##.....#..
+##.##..#.##.#..#####.#.#.
+#....#.######.#.#.#.##.#.
+###.##.#.######.#..###.#.
+#...###.#.#..##..####....
+###...##.###..###..##..#.
+..##.###...#.....##.##.##
+..##..#.###.###.....#.###
+#..###.##.#.###......####
+#.#...#..##.###.....##.#.
+#..#.##...##.##....#...#.
+..#.#..#..#...##.#..###..
+......###....#.....#....#";
+
+enum Direction {
+  Left,
+  Up,
+  Right,
+  Down
+}
 
 struct Carrier {
-  // position: (i32, i32),
-  // direction: Direction
+  position: (i32, i32),
+  direction: Direction
 }
 
 impl Carrier {
-  pub fn new(_grid: &[Vec<bool>]) -> Carrier {
+  pub fn new() -> Carrier {
     Carrier {
-      // position: (
-      //   ((grid.len() + 1) / 2) as i32,
-      //   ((grid[0].len() + 1) / 2) as i32
-      // ),
-      // direction: Direction::Up
+      position: (0, 0),
+      direction: Direction::Up
     }
   }
 
-  fn step(&self, grid: &[Vec<bool>]) -> Vec<Vec<bool>>{
-    // pass
-    let rv: Vec<Vec<bool>> = grid.to_vec();
-    rv
+  fn step(&mut self, grid: &HashSet<(i32, i32)>) -> (HashSet<(i32, i32)>, bool) {
+    let mut rv = grid.clone();
+    let mut infected = false;
+    if grid.contains(&self.position) {
+      rv.remove(&self.position);
+      self.direction = match self.direction {
+        Direction::Left => Direction::Up,
+        Direction::Up => Direction::Right,
+        Direction::Right => Direction::Down,
+        Direction::Down => Direction::Left
+      }
+    } else {
+      infected = true;
+      rv.insert(self.position.clone());
+      self.direction = match self.direction {
+        Direction::Left => Direction::Down,
+        Direction::Up => Direction::Left,
+        Direction::Right => Direction::Up,
+        Direction::Down => Direction::Right
+      }
+    }
+    self.position = match self.direction {
+      Direction::Left => (self.position.0 - 1, self.position.1),
+      Direction::Up => (self.position.0, self.position.1 - 1),
+      Direction::Right => (self.position.0 + 1, self.position.1),
+      Direction::Down => (self.position.0, self.position.1 + 1)
+    };
+    (rv, infected)
   }
 }
 
-fn process_data_a(data: &str, iterations: usize) -> i32 {
+fn get_cells(data: &str) -> HashSet<(i32, i32)> {
   let mut grid: Vec<Vec<bool>> = Vec::new();
   for line in data.lines() {
     let mut row = Vec::new();
@@ -44,16 +91,51 @@ fn process_data_a(data: &str, iterations: usize) -> i32 {
     }
     grid.push(row);
   }
-  let carrier = Carrier::new(&grid);
-  println!("{:?}", grid);
-  for _ in 0..iterations {
-    grid = carrier.step(&grid);
+  let mid_y = (grid.len() / 2) as i32;
+  let mid_x = (grid[0].len() / 2) as i32;
+  // println!("{}x{}", mid_x, mid_y);
+
+  let mut rv = HashSet::new();
+  for (y, line) in grid.iter().enumerate() {
+    for (x, cell) in line.iter().enumerate() {
+      if *cell {
+        rv.insert((x as i32 - mid_x, y as i32 - mid_y));
+      }
+    }
   }
-  0
+  rv
+}
+
+fn process_data_a(data: &str, iterations: usize) -> usize {
+  let mut cells = get_cells(data);
+  let mut carrier = Carrier::new();
+  // println!("{:?}", cells);
+  let mut rv = 0;
+  for _ in 0..iterations {
+    let temp = carrier.step(&cells);
+    cells = temp.0;
+    if temp.1 {
+      rv += 1;
+    }
+    // println!("\n{:?}", cells);
+  }
+  rv
 }
 
 fn process_data_b(_data: &str) -> i32 {
-  0
+  let mut cells = get_cells(data);
+  let mut carrier = Carrier::new();
+  // println!("{:?}", cells);
+  let mut rv = 0;
+  for _ in 0..iterations {
+    let temp = carrier.step(&cells);
+    cells = temp.0;
+    if temp.1 {
+      rv += 1;
+    }
+    // println!("\n{:?}", cells);
+  }
+  rv
 }
 
 //-----------------------------------------------------
