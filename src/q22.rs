c5c6865b5284e4d5a826553b6d6444851027bc32
Blake Winton <bwinton@latte.ca>
2017-12-25 15:34:17 -0500

    First cut at day 22.

--- src/q22.rs
+++ src/q22.rs
@@ -3,7 +3,8 @@
 
 use day;
 
-use std::collections::HashSet;
+use std::collections::HashMap;
+use std::str::FromStr;
 
 static INPUT : &'static str = "....##.#.#.#...#.##.##.#.
 ##.####..###..#.#.#.###.#
@@ -31,6 +32,7 @@ static INPUT : &'static str = "....##.#.#.#...#.##.##.#.
 ..#.#..#..#...##.#..###..
 ......###....#.....#....#";
 
+#[derive(Debug)]
 enum Direction {
   Left,
   Up,
@@ -38,6 +40,88 @@ enum Direction {
   Down
 }
 
+#[derive(Clone)]
+#[derive(Debug)]
+#[derive(PartialEq)]
+enum Cell {
+  Clean,
+  Weakened,
+  Infected,
+  Flagged
+}
+
+#[derive(Clone)]
+#[derive(Debug)]
+struct Board{
+  cells: HashMap<(i32, i32), Cell>,
+  simple: bool
+}
+
+impl FromStr for Board {
+  type Err = ();
+
+  fn from_str(data: &str) -> Result<Board, ()> {
+    let mut grid: Vec<Vec<bool>> = Vec::new();
+    for line in data.lines() {
+      let mut row = Vec::new();
+      for cell in line.chars() {
+        row.push(cell == '#');
+      }
+      grid.push(row);
+    }
+    let mid_y = (grid.len() / 2) as i32;
+    let mid_x = (grid[0].len() / 2) as i32;
+    // println!("{}x{}", mid_x, mid_y);
+
+    let mut cells = HashMap::new();
+    for (y, line) in grid.iter().enumerate() {
+      for (x, cell) in line.iter().enumerate() {
+        if *cell {
+          cells.insert((x as i32 - mid_x, y as i32 - mid_y), Cell::Infected);
+        }
+      }
+    }
+
+    Ok(Board {
+      cells: cells,
+      simple: true
+    })
+  }
+}
+
+impl Board {
+  fn step(&mut self, position: (i32, i32)) -> (Cell, Cell) {
+    let mut new_cells = self.cells.clone();
+    let prev = self.cells.get(&position).unwrap_or(&Cell::Clean).clone();
+
+    if let Some(cell_type) = self.cells.get(&position) {
+      if self.simple {
+        new_cells.remove(&position);
+      } else {
+        match *cell_type {
+          Cell::Clean => {
+            panic!("Found clean cell at {:?}", position);
+          },
+          Cell::Weakened => {
+            new_cells.insert(position, Cell::Infected);
+          },
+          Cell::Infected => {
+            new_cells.insert(position, Cell::Flagged);
+          },
+          Cell::Flagged => {
+            new_cells.remove(&position);
+          }
+        }
+      }
+    } else {
+      new_cells.insert(position, if self.simple {Cell::Infected} else {Cell::Weakened});
+    }
+    self.cells = new_cells;
+    (prev, self.cells.get(&position).unwrap_or(&Cell::Clean).clone())
+  }
+}
+
+#[derive(Debug)]
 struct Carrier {
   position: (i32, i32),
   direction: Direction
@@ -51,26 +135,28 @@ impl Carrier {
     }
   }
 
-  fn step(&mut self, grid: &HashSet<(i32, i32)>) -> (HashSet<(i32, i32)>, bool) {
-    let mut rv = grid.clone();
-    let mut infected = false;
-    if grid.contains(&self.position) {
-      rv.remove(&self.position);
-      self.direction = match self.direction {
-        Direction::Left => Direction::Up,
-        Direction::Up => Direction::Right,
-        Direction::Right => Direction::Down,
-        Direction::Down => Direction::Left
-      }
-    } else {
-      infected = true;
-      rv.insert(self.position.clone());
-      self.direction = match self.direction {
+  fn step(&mut self, cell_type: &Cell) {
+    // println!("At {:?}, found {:?}", &self, &cell_type);
+    match *cell_type {
+      Cell::Clean => self.direction = match self.direction {
         Direction::Left => Direction::Down,
         Direction::Up => Direction::Left,
         Direction::Right => Direction::Up,
         Direction::Down => Direction::Right
-      }
+      },
+      Cell::Weakened => {},
+      Cell::Infected => self.direction = match self.direction {
+        Direction::Left => Direction::Up,
+        Direction::Up => Direction::Right,
+        Direction::Right => Direction::Down,
+        Direction::Down => Direction::Left
+      },
+      Cell::Flagged => self.direction = match self.direction {
+        Direction::Left => Direction::Right,
+        Direction::Up => Direction::Down,
+        Direction::Right => Direction::Left,
+        Direction::Down => Direction::Up
+      },
     }
     self.position = match self.direction {
       Direction::Left => (self.position.0 - 1, self.position.1),
@@ -78,43 +164,19 @@ impl Carrier {
       Direction::Right => (self.position.0 + 1, self.position.1),
       Direction::Down => (self.position.0, self.position.1 + 1)
     };
-    (rv, infected)
+    // println!("  Moved to {:?}\n", &self);
   }
-}
-
-fn get_cells(data: &str) -> HashSet<(i32, i32)> {
-  let mut grid: Vec<Vec<bool>> = Vec::new();
-  for line in data.lines() {
-    let mut row = Vec::new();
-    for cell in line.chars() {
-      row.push(cell == '#');
-    }
-    grid.push(row);
-  }
-  let mid_y = (grid.len() / 2) as i32;
-  let mid_x = (grid[0].len() / 2) as i32;
-  // println!("{}x{}", mid_x, mid_y);
-
-  let mut rv = HashSet::new();
-  for (y, line) in grid.iter().enumerate() {
-    for (x, cell) in line.iter().enumerate() {
-      if *cell {
-        rv.insert((x as i32 - mid_x, y as i32 - mid_y));
-      }
-    }
-  }
-  rv
 }
 
 fn process_data_a(data: &str, iterations: usize) -> usize {
-  let mut cells = get_cells(data);
+  let mut cells: Board = data.parse().unwrap();
   let mut carrier = Carrier::new();
   // println!("{:?}", cells);
   let mut rv = 0;
   for _ in 0..iterations {
-    let temp = carrier.step(&cells);
-    cells = temp.0;
-    if temp.1 {
+    let cell_type = cells.step(carrier.position);
+    carrier.step(&cell_type.0);
+    if cell_type.1 == Cell::Infected {
       rv += 1;
     }
     // println!("\n{:?}", cells);
@@ -122,15 +184,16 @@ fn process_data_a(data: &str, iterations: usize) -> usize {
   rv
 }
 
-fn process_data_b(_data: &str) -> i32 {
-  let mut cells = get_cells(data);
+fn process_data_b(data: &str, iterations: usize) -> usize {
+  let mut cells: Board = data.parse().unwrap();
+  cells.simple = false;
   let mut carrier = Carrier::new();
   // println!("{:?}", cells);
   let mut rv = 0;
   for _ in 0..iterations {
-    let temp = carrier.step(&cells);
-    cells = temp.0;
-    if temp.1 {
+    let cell_type = cells.step(carrier.position);
+    carrier.step(&cell_type.0);
+    if cell_type.1 == Cell::Infected {
       rv += 1;
     }
     // println!("\n{:?}", cells);
@@ -156,7 +219,7 @@ impl day::Day for Q {
 
   fn b(&self) {
     print!("{}B: ", self.number());
-    let result = process_data_b(INPUT);
+    let result = process_data_b(INPUT, 10_000_000);
     println!("Result = {}", result);
   }
 }
@@ -166,15 +229,23 @@ fn a() {
   assert_eq!(process_data_a("..#
 #..
 ...", 7), 5);
-assert_eq!(process_data_a("..#
+  assert_eq!(process_data_a("..#
 #..
 ...", 70), 41);
-assert_eq!(process_data_a("..#
+  assert_eq!(process_data_a("..#
 #..
 ...", 10_000), 5587);
 }
 
 #[test]
 fn b() {
-  assert_eq!(process_data_b(""), 0);
+  assert_eq!(process_data_b("..#
+#..
+...", 7), 1);
+  assert_eq!(process_data_b("..#
+#..
+...", 100), 26);
+//   assert_eq!(process_data_b("..#
+// #..
+// ...", 10_000_000), 2_511_944);
 }
