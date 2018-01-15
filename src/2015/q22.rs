4b3d7f71ce0ffc7c3a76a77981b7527256bdadc9
Blake Winton <bwinton@latte.ca>
2018-01-15 13:33:20 -0500

    Start day 22.

---
+++ src/2015/q22.rs
@@ -0,0 +1,47 @@
+//-----------------------------------------------------
+// Setup.
+
+use aoc::Day;
+
+static INPUT : &'static str = "";
+
+fn process_data_a(_data: &str) -> i32 {
+  0
+}
+
+fn process_data_b(_data: &str) -> i32 {
+  0
+}
+
+//-----------------------------------------------------
+// Questions.
+
+pub struct Q;
+
+impl Day for Q {
+  fn number(&self) -> String {
+    String::from("22")
+  }
+
+  fn a(&self) {
+    print!("{}A: ", self.number());
+    let result = process_data_a(INPUT);
+    println!("Result = {}", result);
+  }
+
+  fn b(&self) {
+    print!("{}B: ", self.number());
+    let result = process_data_b(INPUT);
+    println!("Result = {}", result);
+  }
+}
+
+#[test]
+fn a() {
+  assert_eq!(process_data_a(""), 0);
+}
+
+#[test]
+fn b() {
+  assert_eq!(process_data_b(""), 0);
+}
