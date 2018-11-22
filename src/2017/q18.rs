50e09e5d869c08ac1a0854a4a95f4f6d9a4f7497
Blake Winton <bwinton@latte.ca>
2018-11-22 11:30:12 -0500

    Move 2017 data to a data directory.

--- src/2017/q18.rs
+++ src/2017/q18.rs
@@ -7,47 +7,7 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::str::FromStr;
 
-static INPUT: &'static str = "set i 31
-set a 1
-mul p 17
-jgz p p
-mul a 2
-add i -1
-jgz i -2
-add a -1
-set i 127
-set p 316
-mul p 8505
-mod p a
-mul p 129749
-add p 12345
-mod p a
-set b p
-mod b 10000
-snd b
-add i -1
-jgz i -9
-jgz a 3
-rcv b
-jgz b -1
-set f 0
-set i 126
-rcv a
-rcv b
-set p a
-mul p -1
-add p b
-jgz p 4
-snd a
-set a b
-jgz 1 3
-snd b
-set f 1
-add i -1
-jgz i -11
-snd a
-jgz f -16
-jgz a -19";
+static INPUT: &'static str = include_str!("data/q18.data");
 
 #[derive(Clone)]
 #[derive(Debug)]
--- src/2017/q23.rs
+++ src/2017/q23.rs
@@ -7,38 +7,7 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::str::FromStr;
 
-static INPUT: &'static str = "set b 57
-set c b
-jnz a 2
-jnz 1 5
-mul b 100
-sub b -100000
-set c b
-sub c -17000
-set f 1
-set d 2
-set e 2
-set g d
-mul g e
-sub g b
-jnz g 2
-set f 0
-sub e -1
-set g e
-sub g b
-jnz g -8
-sub d -1
-set g d
-sub g b
-jnz g -13
-jnz f 2
-sub h -1
-set g b
-sub g c
-jnz g 2
-jnz 1 3
-sub b -17
-jnz 1 -23";
+static INPUT: &'static str = include_str!("data/q23.data");
 
 #[derive(Clone)]
 #[derive(Debug)]
