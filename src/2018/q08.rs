b9e93c25e458577f5483927f7b0a051b03d96f23
Blake Winton <bwinton@latte.ca>
2018-12-08 10:17:08 -0500

    Finished day 8.

--- src/2018/q08.rs
+++ src/2018/q08.rs
@@ -3,18 +3,80 @@
 
 static INPUT : &'static str = include_str!("data/q08.data");
 
-fn process_data_a(_data: &str) -> i32 {
-    0
+// #[derive(Clone,Debug)]
+// struct Node {
+//     child_count: i32,
+//     metadata: i32,
+//     children: Vec<Box<Node>>,
+// }
+
+fn get_metadata_a(numbers: &mut Vec<i32>) -> i32 {
+    if numbers.is_empty() {
+        return 0;
+    }
+
+    let child_count = numbers.pop().unwrap();
+    let metadata_count = numbers.pop().unwrap();
+
+    let mut metadata = 0;
+    for _ in 0..child_count {
+        metadata += get_metadata_a(numbers);
+    }
+    for _ in 0..metadata_count {
+        metadata += numbers.pop().unwrap();
+    }
+    metadata
 }
 
-fn process_data_b(_data: &str) -> i32 {
-    0
+fn get_metadata_b(numbers: &mut Vec<i32>) -> i32 {
+    if numbers.is_empty() {
+        return 0;
+    }
+
+    let child_count = numbers.pop().unwrap();
+    let metadata_count = numbers.pop().unwrap();
+
+    let mut metadata = 0;
+    if child_count == 0 {
+        for _ in 0..metadata_count {
+            metadata += numbers.pop().unwrap();
+        }
+    } else {
+        let mut children = vec![];
+        for _ in 0..child_count {
+            children.push(get_metadata_b(numbers))
+        }
+        let mut indices = vec![];
+        for _ in 0..metadata_count {
+            indices.push(numbers.pop().unwrap());
+        }
+        for i in indices {
+            let index = i - 1;
+            if let Some(child) = children.get(index as usize) {
+                metadata += child;
+            }
+        }
+
+    }
+    metadata
+}
+
+fn process_data_a(data: &str) -> i32 {
+    let mut numbers: Vec<i32> = data.split(" ").map(|x| x.parse().unwrap()).collect();
+    numbers.reverse();
+    get_metadata_a(&mut numbers)
+}
+
+fn process_data_b(data: &str) -> i32 {
+    let mut numbers: Vec<i32> = data.split(" ").map(|x| x.parse().unwrap()).collect();
+    numbers.reverse();
+    get_metadata_b(&mut numbers)
 }
 
 //-----------------------------------------------------
 // Questions.
 
-q_impl!("08");
+q_impl!("8");
 
 #[test]
 fn a() {
@@ -23,5 +85,5 @@ fn a() {
 
 #[test]
 fn b() {
-    assert_eq!(process_data_b(""), 0);
+    assert_eq!(process_data_b("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"), 66);
 }
