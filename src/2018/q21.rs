6dede5787218bf9ea940672bcd1410310a85caa8
Blake Winton <bwinton@latte.ca>
2018-12-21 19:47:06 -0500

    Day 21. Needed a lot of help on this one.

--- src/2018/q21.rs
+++ src/2018/q21.rs
@@ -1,14 +1,60 @@
 //-----------------------------------------------------
 // Setup.
 
+use std::collections::HashSet;
+
 static INPUT: &'static str = include_str!("data/q21.data");
 
-fn process_data_a(_data: &str) -> i32 {
-    0
+fn step(input: i64) -> i64 {
+    let init = 707129;
+    let perturb = 65899;
+    let mut bytes = input | 0x10000;
+    let mut hash = init;
+    while bytes != 0 {
+        hash += bytes & 0xFF;
+        hash &= 0xFFFFFF;
+        hash *= perturb;
+        hash &= 0xFFFFFF;
+        bytes >>= 8;
+    }
+    hash
 }
 
-fn process_data_b(_data: &str) -> i32 {
-    0
+fn process_data_a(_data: &str) -> i64 {
+    step(0)
+}
+
+fn process_data_b(_data: &str) -> i64 {
+    let mut results = HashSet::new();
+    let mut last_result = -1;
+    let mut r5 = 0;
+    loop {
+        let mut r3 = r5 | 65_536;
+        r5 = 707129;
+        loop {
+            r5 += r3 & 255;
+            r5 &= 16_777_215;
+            r5 *= 65899;
+            r5 &= 16_777_215;
+            if 256 > r3 {
+                break;
+            }
+            let mut r2 = 0;
+            while (r2 + 1) * 256 <= r3 {
+                r2 += 1;
+            }
+            r3 = r2;
+        }
+
+        if !results.contains(&r5) {
+            results.insert(r5);
+            last_result = r5;
+        } else {
+            return last_result;
+        }
+    }
+    // 16776149 is too high.
+    // 12502875
 }
 
 //-----------------------------------------------------
