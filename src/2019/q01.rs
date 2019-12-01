16262d3bb6790dc5fb063b4472283a8fcfdd48b2
Blake Winton <bwinton@latte.ca>
2019-12-01 00:09:08 -0500

    Finish day 1!

--- src/2019/q01.rs
+++ src/2019/q01.rs
@@ -3,12 +3,26 @@
 
 static INPUT: &str = include_str!("data/q01.data");
 
-fn process_data_a(_data: &str) -> i32 {
-    0
+fn process_data_a(data: &str) -> u32 {
+    let mut sum = 0;
+    for line in data.lines() {
+        let weight: u32 = line.parse().unwrap();
+        sum += weight / 3 - 2;
+    }
+    sum
 }
 
-fn process_data_b(_data: &str) -> i32 {
-    0
+fn process_data_b(data: &str) -> u32 {
+    let mut sum = 0;
+    for line in data.lines() {
+        let weight: u32 = line.parse().unwrap();
+        let mut fuel: i32 = weight as i32 / 3 - 2;
+        while fuel > 0 {
+            sum += fuel as u32;
+            fuel = fuel as i32 / 3 - 2;
+        }
+    }
+    sum
 }
 
 //-----------------------------------------------------
@@ -18,10 +32,26 @@ q_impl!("1");
 
 #[test]
 fn a() {
-    assert_eq!(process_data_a(""), 0);
+    assert_eq!(process_data_a("12"), 2);
+    assert_eq!(process_data_a("14"), 2);
+    assert_eq!(
+        process_data_a(
+            "12
+14"
+        ),
+        4
+    );
 }
 
 #[test]
 fn b() {
-    assert_eq!(process_data_b(""), 0);
+    assert_eq!(process_data_b("14"), 2);
+    assert_eq!(process_data_b("1969"), 966);
+    assert_eq!(
+        process_data_b(
+            "14
+1969"
+        ),
+        968
+    );
 }
