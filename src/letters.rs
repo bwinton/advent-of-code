d4f5d64496350b372a6be296aa17418c64b81f86
Blake Winton <bwinton@latte.ca>
2019-12-14 18:34:23 -0500

    Add Y!

--- src/letters.rs
+++ src/letters.rs
@@ -1,249 +1,249 @@
 use std::collections::HashMap;
 
 #[rustfmt::skip]
-//  ██  ███   ██       ████ ████  ██  █  █        ██ █  █ █             ██  ███      ███   ███      █  █                     ████
-// █  █ █  █ █  █      █    █    █  █ █  █         █ █ █  █            █  █ █  █     █  █ █         █  █                        █
-// █  █ ███  █         ███  ███  █    ████         █ ██   █            █  █ █  █     █  █ █         █  █                       █ 
-// ████ █  █ █         █    █    █ ██ █  █         █ █ █  █            █  █ ███      ███   ██       █  █                      █  
-// █  █ █  █ █  █      █    █    █  █ █  █      █  █ █ █  █            █  █ █        █ █     █      █  █                     █   
-// █  █ ███   ██       ████ █     ███ █  █       ██  █  █ ████          ██  █        █  █ ███        ██                      ████
+//  ██  ███   ██       ████ ████  ██  █  █        ██ █  █ █             ██  ███      ███   ███      █  █                █   █████
+// █  █ █  █ █  █      █    █    █  █ █  █         █ █ █  █            █  █ █  █     █  █ █         █  █                █   █   █
+// █  █ ███  █         ███  ███  █    ████         █ ██   █            █  █ █  █     █  █ █         █  █                 █ █   █ 
+// ████ █  █ █         █    █    █ ██ █  █         █ █ █  █            █  █ ███      ███   ██       █  █                  █   █  
+// █  █ █  █ █  █      █    █    █  █ █  █      █  █ █ █  █            █  █ █        █ █     █      █  █                  █  █   
+// █  █ ███   ██       ████ █     ███ █  █       ██  █  █ ████          ██  █        █  █ ███        ██                   █  ████
 
 #[rustfmt::skip]
 lazy_static! {
     static ref LETTERS: HashMap<Vec<bool>, char> = {
         let mut letters = HashMap::new();
         letters.insert(vec![
-            false, true, true, false, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, true, true, true, 
-            true, false, false, true, 
-            true, false, false, true],
+            false, true, true, false, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, true, true, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false],
             'A');
         
         letters.insert(vec![
-            true, true, true, false, 
-            true, false, false, true, 
-            true, true, true, false, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, true, true, false],
+            true, true, true, false, false,
+            true, false, false, true, false,
+            true, true, true, false, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, true, true, false, false],
             'B');
         
         letters.insert(vec![
-            false, true, true, false, 
-            true, false, false, true, 
-            true, false, false, false, 
-            true, false, false, false, 
-            true, false, false, true, 
-            false, true, true, false],
+            false, true, true, false, false,
+            true, false, false, true, false,
+            true, false, false, false, false,
+            true, false, false, false, false,
+            true, false, false, true, false,
+            false, true, true, false, false],
             'C');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'D');
         
         letters.insert(vec![
-            true, true, true, true, 
-            true, false, false, false, 
-            true, true, true, false, 
-            true, false, false, false, 
-            true, false, false, false, 
-            true, true, true, true],
+            true, true, true, true, false,
+            true, false, false, false, false,
+            true, true, true, false, false,
+            true, false, false, false, false,
+            true, false, false, false, false,
+            true, true, true, true, false],
             'E');
         
         letters.insert(vec![
-            true, true, true, true, 
-            true, false, false, false, 
-            true, true, true, false, 
-            true, false, false, false, 
-            true, false, false, false, 
-            true, false, false, false],
+            true, true, true, true, false,
+            true, false, false, false, false,
+            true, true, true, false, false,
+            true, false, false, false, false,
+            true, false, false, false, false,
+            true, false, false, false, false],
             'F');
         
         letters.insert(vec![
-            false, true, true, false, 
-            true, false, false, true, 
-            true, false, false, false, 
-            true, false, true, true, 
-            true, false, false, true, 
-            false, true, true, true],
+            false, true, true, false, false,
+            true, false, false, true, false,
+            true, false, false, false, false,
+            true, false, true, true, false,
+            true, false, false, true, false,
+            false, true, true, true, false],
             'G');
         
         letters.insert(vec![
-            true, false, false, true, 
-            true, false, false, true, 
-            true, true, true, true, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, false, false, true],
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, true, true, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false],
             'H');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'I');
         
         letters.insert(vec![
-            false, false, true, true, 
-            false, false, false, true, 
-            false, false, false, true, 
-            false, false, false, true, 
-            true, false, false, true, 
-            false, true, true, false],
+            false, false, true, true, false,
+            false, false, false, true, false,
+            false, false, false, true, false,
+            false, false, false, true, false,
+            true, false, false, true, false,
+            false, true, true, false, false],
             'J');
         
         letters.insert(vec![
-            true, false, false, true, 
-            true, false, true, false, 
-            true, true, false, false, 
-            true, false, true, false, 
-            true, false, true, false, 
-            true, false, false, true],
+            true, false, false, true, false,
+            true, false, true, false, false,
+            true, true, false, false, false,
+            true, false, true, false, false,
+            true, false, true, false, false,
+            true, false, false, true, false],
             'K');
         
         letters.insert(vec![
-            true, false, false, false, 
-            true, false, false, false, 
-            true, false, false, false, 
-            true, false, false, false, 
-            true, false, false, false, 
-            true, true, true, true],
+            true, false, false, false, false,
+            true, false, false, false, false,
+            true, false, false, false, false,
+            true, false, false, false, false,
+            true, false, false, false, false,
+            true, true, true, true, false],
             'L');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'M');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'N');
         
         letters.insert(vec![
-            false, true, true, false, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, false, false, true, 
-            false, true, true, false],
+            false, true, true, false, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            false, true, true, false, false],
             'O');
         
         letters.insert(vec![
-            true, true, true, false, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, true, true, false, 
-            true, false, false, false, 
-            true, false, false, false],
+            true, true, true, false, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, true, true, false, false,
+            true, false, false, false, false,
+            true, false, false, false, false],
             'P');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'Q');
         
         letters.insert(vec![
-            true, true, true, false, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, true, true, false, 
-            true, false, true, false, 
-            true, false, false, true],
+            true, true, true, false, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, true, true, false, false,
+            true, false, true, false, false,
+            true, false, false, true, false],
             'R');
         
         letters.insert(vec![
-            false, true, true, true, 
-            true, false, false, false, 
-            true, false, false, false, 
-            false, true, true, false, 
-            false, false, false, true, 
-            true, true, true, false],
+            false, true, true, true, false,
+            true, false, false, false, false,
+            true, false, false, false, false,
+            false, true, true, false, false,
+            false, false, false, true, false,
+            true, true, true, false, false],
             'S');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'T');
         
         letters.insert(vec![
-            true, false, false, true, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, false, false, true, 
-            true, false, false, true, 
-            false, true, true, false],
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            true, false, false, true, false,
+            false, true, true, false, false],
             'U');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'V');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'W');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false,
+            false, false, false, false, false],
             'X');
         
         letters.insert(vec![
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false, 
-            false, false, false, false],
+            true, false, false, false, true,
+            true, false, false, false, true,
+            false, true, false, true, false,
+            false, false, true, false, false,
+            false, false, true, false, false,
+            false, false, true, false, false],
             'Y');
         
         letters.insert(vec![
-            true, true, true, true, 
-            false, false, false, true, 
-            false, false, true, false, 
-            false, true, false, false, 
-            true, false, false, false, 
-            true, true, true, true],
+            true, true, true, true, false,
+            false, false, false, true, false,
+            false, false, true, false, false,
+            false, true, false, false, false,
+            true, false, false, false, false,
+            true, true, true, true, false],
             'Z');
         letters
     };
@@ -268,7 +268,7 @@ pub fn recognize_letters(image: &[bool]) -> String {
     for i in 0..char_count {
         let mut letter = vec![];
         for row in 0..6 {
-            for column in 0..4 {
+            for column in 0..5 {
                 // print!("{}", if image[offset + row * column_count + column + 5 * i] { '█' } else { ' ' });
                 letter.push(image[offset + row * column_count + column + 5 * i]);
             }
