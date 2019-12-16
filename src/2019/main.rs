fa6b59b6eef9535db15eec011eaefec69441dc6d
Blake Winton <bwinton@latte.ca>
2019-12-16 01:56:33 -0500

    Set up for day 17.

--- src/2019/main.rs
+++ src/2019/main.rs
@@ -18,7 +18,7 @@ mod q13;
 mod q14;
 mod q15;
 mod q16;
-// mod q17;
+mod q17;
 // mod q18;
 // mod q19;
 // mod q20;
@@ -43,8 +43,8 @@ extern crate test;
 
 fn main() {
     let days = q_vec!(
-        q01, q02, q03, q04, q05, q06, q07, q08, q09, q10, q11, q12, q13, q14, q15,
-        q16 // , q17, q18, q19, q20, q21, q22, q23, q24, q25
+        q01, q02, q03, q04, q05, q06, q07, q08, q09, q10, q11, q12, q13, q14, q15, q16,
+        q17 //, q18, q19, q20, q21, q22, q23, q24, q25
     );
 
     aoc::main(&days)
