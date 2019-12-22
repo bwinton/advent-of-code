c17851b6479c1c8ca93ea1f5d0484579535f1d52
Blake Winton <bwinton@latte.ca>
2019-12-21 23:27:46 -0500

    Finish day 21.

--- src/2019/q21.rs
+++ src/2019/q21.rs
@@ -1,14 +1,78 @@
 //-----------------------------------------------------
 // Setup.
 
+use crate::intcode::{Intcode, State};
+
 static INPUT: &str = include_str!("data/q21.data");
 
-fn process_data_a(_data: &str) -> i32 {
-    0
+fn process_data_a(data: &str) -> i128 {
+    let ints: Vec<i128> = data
+        .split(',')
+        .map(|i| i.parse::<i128>().unwrap())
+        .collect();
+    let program = "NOT A T
+NOT B J
+OR J T
+NOT C J
+OR J T
+NOT D J
+NOT J J
+AND T J
+WALK
+";
+    let mut machine = Intcode::new(ints, program.chars().map(|x| x as i128).collect());
+    match machine.run_tape() {
+        Ok(State::Halted) => {}
+        Err(code) => {
+            println!("ERROR!!! {}", code);
+        }
+        Ok(state) => {
+            println!("ERROR, machine not halted! {:?}", state);
+        }
+    }
+    let outputs = &mut machine.outputs;
+    // for output in outputs.iter().rev() {
+    //     print!("{}", *output as u8 as char);
+    // }
+    println!("{:?}", outputs);
+    outputs[0]
 }
 
-fn process_data_b(_data: &str) -> i32 {
-    0
+fn process_data_b(data: &str) -> i128 {
+    let ints: Vec<i128> = data
+        .split(',')
+        .map(|i| i.parse::<i128>().unwrap())
+        .collect();
+    let program = "NOT A T
+NOT B J
+OR J T
+NOT C J
+OR J T
+NOT D J
+NOT J J
+AND J T
+NOT H J
+NOT J J
+OR E J
+AND T J
+RUN
+";
+    let mut machine = Intcode::new(ints, program.chars().map(|x| x as i128).collect());
+    match machine.run_tape() {
+        Ok(State::Halted) => {}
+        Err(code) => {
+            println!("ERROR!!! {}", code);
+        }
+        Ok(state) => {
+            println!("ERROR, machine not halted! {:?}", state);
+        }
+    }
+    let outputs = &mut machine.outputs;
+    for output in outputs.iter().rev() {
+        print!("{}", *output as u8 as char);
+    }
+    // println!("{:?}", outputs);
+    outputs[0]
 }
 
 //-----------------------------------------------------
