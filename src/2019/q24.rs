38a2c724884cb7ec319edf1bfe78d6b4c61081c0
Blake Winton <bwinton@latte.ca>
2019-12-23 10:51:55 -0500

    Set up day 24 and 25.

---
+++ src/2019/q24.rs
@@ -0,0 +1,27 @@
+//-----------------------------------------------------
+// Setup.
+
+static INPUT: &str = include_str!("data/q24.data");
+
+fn process_data_a(_data: &str) -> i32 {
+    0
+}
+
+fn process_data_b(_data: &str) -> i32 {
+    0
+}
+
+//-----------------------------------------------------
+// Questions.
+
+q_impl!("24");
+
+#[test]
+fn a() {
+    assert_eq!(process_data_a(""), 0);
+}
+
+#[test]
+fn b() {
+    assert_eq!(process_data_b(""), 0);
+}
