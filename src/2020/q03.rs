82c9210948cdc5064d8676f8d69b29da26b8526f
Blake Winton <bwinton@latte.ca>
2020-12-03 00:17:17 -0500

    Day 3.

--- src/2020/q03.rs
+++ src/2020/q03.rs
@@ -3,12 +3,39 @@
 
 static INPUT: &str = include_str!("data/q03.data");
 
-fn process_data_a(_data: &str) -> usize {
-    0
+fn process(data: &str, values: &[(usize, usize)]) -> usize {
+    let mut map: Vec<Vec<bool>> = vec![];
+    for line in data.lines() {
+        // Do something
+        let mut curr = vec![];
+        for cell in line.chars() {
+            curr.push(cell == '#');
+        }
+        map.push(curr);
+    }
+    let mut rv = 1;
+    for slope in values {
+        let mut pos = (0, 0);
+        let mut curr = 0;
+        while pos.1 < map.len() {
+            if map[pos.1][pos.0] {
+                curr += 1;
+            }
+            pos.0 = (pos.0 + slope.0) % map[0].len();
+            pos.1 += slope.1;
+        }
+        // println!("{:?}, {}", slope, curr);
+        rv *= curr;
+    }
+    rv
 }
 
-fn process_data_b(_data: &str) -> usize {
-    0
+fn process_data_a(data: &str) -> usize {
+    process(data, &[(3, 1)])
+}
+
+fn process_data_b(data: &str) -> usize {
+    process(data, &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
 }
 
 //-----------------------------------------------------
@@ -18,10 +45,40 @@ q_impl!("3");
 
 #[test]
 fn a() {
-    assert_eq!(process_data_a(""), 0);
+    assert_eq!(
+        process_data_a(
+            "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+        ),
+        7
+    );
 }
 
 #[test]
 fn b() {
-    assert_eq!(process_data_b(""), 0);
+    assert_eq!(
+        process_data_b(
+            "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"
+        ),
+        336
+    );
 }
