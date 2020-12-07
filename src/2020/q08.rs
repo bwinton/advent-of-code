d1d907f991c054c95f741e8d40f0f8aaf6dbf1d9
Blake Winton <bwinton@latte.ca>
2020-12-07 09:37:16 -0500

    Set up for day 8.

---
+++ src/2020/q08.rs
@@ -0,0 +1,37 @@
+//-----------------------------------------------------
+// Setup.
+
+static INPUT: &str = include_str!("data/q08.data");
+
+fn process_data_a(data: &str) -> usize {
+    let mut rv = 0;
+    for line in data.lines() {
+        // Do something
+        rv += line.len();
+    }
+    rv
+}
+
+fn process_data_b(data: &str) -> usize {
+    let mut rv = 0;
+    for line in data.lines() {
+        // Do something
+        rv += line.len();
+    }
+    rv
+}
+
+//-----------------------------------------------------
+// Questions.
+
+q_impl!("8");
+
+#[test]
+fn a() {
+    assert_eq!(process_data_a(""), 0);
+}
+
+#[test]
+fn b() {
+    assert_eq!(process_data_b(""), 0);
+}
