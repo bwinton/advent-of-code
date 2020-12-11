6eac0466ff46aca5f61911689ead05b53bb3d2f7
Blake Winton <bwinton@latte.ca>
2020-12-11 15:29:28 -0500

    Set up for day 12.

--- src/2020/main.rs
+++ src/2020/main.rs
@@ -12,7 +12,7 @@ mod q08;
 mod q09;
 mod q10;
 mod q11;
-// mod q12;
+mod q12;
 // mod q13;
 // mod q14;
 // mod q15;
@@ -42,8 +42,8 @@ extern crate lazy_static;
 
 fn main() {
     let days = q_vec!(
-        q01, q02, q03, q04, q05, q06, q07, q08, q09, q10,
-        q11, // q12, q13, q14, q15, q16, q17, q18,
+        q01, q02, q03, q04, q05, q06, q07, q08, q09, q10, q11,
+        q12, // q13, q14, q15, q16, q17, q18,
              // q19, q20, q21, q22, q23, q24, q25
     );
 
