c381313220ebd460071887062f53670b3e464f25
Blake Winton <bwinton@latte.ca>
2020-12-25 10:23:31 -0500

    And we’re done for the year!

--- src/2020/q25.rs
+++ src/2020/q25.rs
@@ -4,21 +4,32 @@
 static INPUT: &str = include_str!("data/q25.data");
 
 fn process_data_a(data: &str) -> usize {
-    let mut rv = 0;
-    for line in data.lines() {
-        // Do something
-        rv += line.len();
+    let mut lines = data.lines();
+    let card_key: usize = lines.next().unwrap().parse().unwrap();
+    let door_key: usize = lines.next().unwrap().parse().unwrap();
+
+    let mut value = 1;
+    let mut subject_number = 7;
+    let mut loop_size = 0;
+    while value != card_key {
+        value *= subject_number;
+        value %= 20201227;
+        loop_size += 1;
     }
-    rv
+    // println!("card loop size = {}", loop_size);
+
+    value = 1;
+    subject_number = door_key;
+    for _ in 0..loop_size {
+        value *= subject_number;
+        value %= 20201227;
+    }
+    // println!("Final value = {}", value);
+    value
 }
 
-fn process_data_b(data: &str) -> usize {
-    let mut rv = 0;
-    for line in data.lines() {
-        // Do something
-        rv += line.len();
-    }
-    rv
+fn process_data_b(_data: &str) -> usize {
+    0
 }
 
 //-----------------------------------------------------
@@ -28,7 +39,13 @@ q_impl!("25");
 
 #[test]
 fn a() {
-    assert_eq!(process_data_a(""), 0);
+    assert_eq!(
+        process_data_a(
+            "5764801
+17807724"
+        ),
+        14_897_079
+    );
 }
 
 #[test]
