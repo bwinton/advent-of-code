1f2ac6f25eabac13820317a7a6be360f31176163
Blake Winton <bwinton@latte.ca>
2021-12-03 13:30:55 -0500

    First cut at downloading the input.

--- src/next.rs
+++ src/next.rs
@@ -1,22 +1,32 @@
 use std::{
+    env::{var, VarError},
     fs::{copy, create_dir_all, read, read_dir, File},
     io::{stdout, Stdout, Write},
     time::SystemTimeError,
 };
 
+use chrono::{Datelike, Duration, Local};
 use clap::{app_from_crate, Arg};
 use crossterm::{style::Print, ExecutableCommand};
 use custom_error::custom_error;
+use reqwest::{
+    blocking::{Client, Response},
+    header::COOKIE,
+};
 
 custom_error! { NextError
     Request{source: reqwest::Error} = "request error",
     Time{source: SystemTimeError} = "time error",
     JSON{source: serde_json::error::Error} = "json error",
-    YearNotFound{} = "Years not found in directories.",
+    YearsNotFound{} = "Years not found in directories.",
+    YearNotFound{year:i32} = "Year {year} not found on server.",
+    InputNotFound{year:i32, day:u32} = "Input \"https://adventofcode.com/{year}/day/{day}/input\" not found on server.",
     ParseIntError{source: std::num::ParseIntError} = "Couldn't parse int",
     CrossTerm{source: crossterm::ErrorKind} = "crossterm error",
     FromUtf8Error{source: std::string::FromUtf8Error} = "Couldn't read input",
     ModulesNotFound{location: String} = "Modules {location} not found in main",
+    Session{source: VarError} = "Could not find AOC_SESSION env variable.",
+    DurationError{duration: Duration} = "Sleep Duration {duration} out of range.",
 }
 
 fn get_last() -> Result<(u32, String), NextError> {
@@ -32,7 +42,7 @@ fn get_last() -> Result<(u32, String), NextError> {
     let last_year = entries
         .into_iter()
         .last()
-        .ok_or(NextError::YearNotFound {})?;
+        .ok_or(NextError::YearsNotFound {})?;
 
     let mut entries = read_dir(format!("src/{}", last_year))?
         .filter_map(|res| {
@@ -49,7 +59,7 @@ fn get_last() -> Result<(u32, String), NextError> {
     let last_day = entries
         .into_iter()
         .last()
-        .ok_or(NextError::YearNotFound {})?;
+        .ok_or(NextError::YearsNotFound {})?;
     Ok((last_year, last_day))
 }
 
@@ -92,7 +102,7 @@ fn add_day(last_year: u32, day: u32, stdout: &mut Stdout) -> Result<(), NextErro
             .ok_or(NextError::ModulesNotFound {
                 location: "qvec end".to_owned(),
             })?;
-    let main = format!("{}q{:02},{}", &main[..next], day, &main[next..]);
+    let main = format!("{} q{:02},{}", &main[..next], day, &main[next..]);
 
     let mut main_out = File::create(format!("src/{}/main.rs", last_year))?;
     main_out.write_all(main.as_bytes())?;
@@ -121,6 +131,65 @@ fn add_next(last_year: u32, last_day: &str, stdout: &mut Stdout) -> Result<(), N
     Ok(())
 }
 
+fn get_url(year: i32, day: u32) -> Result<Response, NextError> {
+    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
+    let mut request = Client::new().get(&url);
+    let session_cookie = format!("session={}", &var("AOC_SESSION")?);
+    request = request.header(COOKIE, session_cookie);
+    let response = request.send()?;
+    Ok(response)
+}
+
+fn get_input() -> Result<(), NextError> {
+    let today = Local::now();
+    let year = today.year();
+    let day = today.day();
+    let datapath = format!("src/{}/data/q{:02}.data", year, day);
+    if std::fs::metadata(&datapath)?.len() == 0 {
+        // We're getting the data for today!
+        println!("Getting input for {:?}", datapath);
+        let mut response = get_url(year, day)?;
+        if !response.status().is_success() {
+            return Err(NextError::InputNotFound { year, day });
+        }
+        let mut file = File::create(&datapath)?;
+        response.copy_to(&mut file)?;
+    } else {
+        // Let's wait and get the data for tomorrow!
+        let day = day + 1;
+        let datapath = format!("src/{}/data/q{:02}.data", year, day);
+        println!("Getting input for {:?}", datapath);
+        let tonight = today
+            .with_day(day)
+            .ok_or(NextError::YearNotFound { year })?
+            .date()
+            .and_hms(0, 0, 1);
+        let duration = tonight - today;
+        println!("  Sleeping for {} until {}", duration, tonight);
+        std::thread::sleep(
+            duration
+                .to_std()
+                .map_err(|_| NextError::DurationError { duration })?,
+        );
+        let mut response = get_url(year, day)?;
+        if !response.status().is_success() {
+            // try again in 5 seconds…
+            std::thread::sleep(
+                Duration::seconds(5)
+                    .to_std()
+                    .map_err(|_| NextError::DurationError { duration })?,
+            );
+            response = get_url(year, day)?;
+            if !response.status().is_success() {
+                return Err(NextError::InputNotFound { year, day });
+            }
+        }
+        let mut file = File::create(&datapath)?;
+        response.copy_to(&mut file)?;
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), NextError> {
     let mut stdout = stdout();
     color_backtrace::install();
@@ -131,7 +200,7 @@ fn main() -> Result<(), NextError> {
                 .about("Which year to add")
                 .long_about("Specify a year to add.")
                 .takes_value(true)
-                .group("input"),
+                .group("arg"),
         )
         .arg(
             Arg::new("day")
@@ -139,7 +208,15 @@ fn main() -> Result<(), NextError> {
                 .about("Which day to add")
                 .long_about("Specify a day to add.")
                 .takes_value(true)
-                .group("input"),
+                .group("arg"),
+        )
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .about("Get input")
+                .long_about("Get input for today.")
+                .takes_value(false)
+                .group("arg"),
         )
         .get_matches();
 
@@ -149,6 +226,8 @@ fn main() -> Result<(), NextError> {
         add_day(last_year, day.parse::<u32>()?, &mut stdout)?;
     } else if let Some(year) = matches.value_of("year") {
         add_year(year.parse::<u32>()?, &mut stdout)?;
+    } else if matches.occurrences_of("input") > 0 {
+        get_input()?;
     } else {
         add_next(last_year, &last_day, &mut stdout)?;
     }
