8d6a18ac86723c8073c5490c42c218a0f66bb203
Blake Winton <bwinton@latte.ca>
2021-12-06 18:23:50 -0500

    Use the built in rotate_left function.

--- src/2021/q06.rs
+++ src/2021/q06.rs
@@ -1,19 +1,7 @@
-use std::collections::HashMap;
-
 //-----------------------------------------------------
 // Setup.
 
 static INPUT: &str = include_str!("data/q06.data");
-
-fn increment(fish: &[u64;9]) -> [u64;9] {
-    let mut rv = [0;9];
-    for i in 0..8 {
-        rv[i] = fish[i + 1];
-    }
-    rv[6] += fish[0];
-    rv[8] += fish[0];
-    rv
-}
 
 fn process_data_a(data: &str) -> u64 {
     let line: Vec<usize> = data.trim().split(',').map(|x| x.parse().unwrap()).collect();
@@ -22,7 +10,8 @@ fn process_data_a(data: &str) -> u64 {
         fish[i] += 1
     }
     for _ in 0..80 {
-        fish = increment(&fish);
+        fish.rotate_left(1);
+        fish[6] += fish[8];
     }
     fish.iter().sum()
 }
@@ -34,7 +23,8 @@ fn process_data_b(data: &str) -> u64 {
         fish[i] += 1
     }
     for _ in 0..256 {
-        fish = increment(&fish);
+        fish.rotate_left(1);
+        fish[6] += fish[8];
     }
     fish.iter().sum()
 }
