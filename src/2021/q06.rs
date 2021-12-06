48e025a47c29deded316409731f72e32148ed40b
Blake Winton <bwinton@latte.ca>
2021-12-06 18:19:57 -0500

    Finish day 6.

--- src/2021/q06.rs
+++ src/2021/q06.rs
@@ -1,24 +1,42 @@
+use std::collections::HashMap;
+
 //-----------------------------------------------------
 // Setup.
 
 static INPUT: &str = include_str!("data/q06.data");
 
-fn process_data_a(data: &str) -> usize {
-    let mut rv = 0;
-    for line in data.lines() {
-        // Do something
-        rv += line.len() * 0;
+fn increment(fish: &[u64;9]) -> [u64;9] {
+    let mut rv = [0;9];
+    for i in 0..8 {
+        rv[i] = fish[i + 1];
     }
+    rv[6] += fish[0];
+    rv[8] += fish[0];
     rv
 }
 
-fn process_data_b(data: &str) -> usize {
-    let mut rv = 0;
-    for line in data.lines() {
-        // Do something
-        rv += line.len() * 0;
+fn process_data_a(data: &str) -> u64 {
+    let line: Vec<usize> = data.trim().split(',').map(|x| x.parse().unwrap()).collect();
+    let mut fish = [0;9];
+    for i in line {
+        fish[i] += 1
     }
-    rv
+    for _ in 0..80 {
+        fish = increment(&fish);
+    }
+    fish.iter().sum()
+}
+
+fn process_data_b(data: &str) -> u64 {
+    let line: Vec<usize> = data.trim().split(',').map(|x| x.parse().unwrap()).collect();
+    let mut fish = [0;9];
+    for i in line {
+        fish[i] += 1
+    }
+    for _ in 0..256 {
+        fish = increment(&fish);
+    }
+    fish.iter().sum()
 }
 
 //-----------------------------------------------------
@@ -28,10 +46,10 @@ q_impl!("6");
 
 #[test]
 fn a() {
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(process_data_a(indoc!("3,4,3,1,2")), 5934);
 }
 
 #[test]
 fn b() {
-    assert_eq!(process_data_b(indoc!("")), 0);
+    assert_eq!(process_data_b(indoc!("3,4,3,1,2")), 26_984_457_539);
 }
