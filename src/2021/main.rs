58b0189cddfa6841a7504aa9edd5b16564ab1991
Blake Winton <bwinton@latte.ca>
2021-12-10 14:10:03 -0500

    And day 10.

--- src/2021/main.rs
+++ src/2021/main.rs
@@ -10,6 +10,8 @@ mod q05;
 mod q06;
 mod q07;
 mod q08;
+mod q09;
+mod q10;
 
 #[macro_use]
 extern crate aoc;
@@ -24,7 +26,7 @@ extern crate itertools;
 extern crate lazy_static;
 
 fn main() {
-    let days = q_vec!(q01, q02, q03, q04, q05, q06, q07, q08,);
+    let days = q_vec!(q01, q02, q03, q04, q05, q06, q07, q08, q09, q10,);
 
     aoc::main(&days)
 }
---
+++ src/2021/q10.rs
@@ -0,0 +1,142 @@
+//-----------------------------------------------------
+// Setup.
+
+static INPUT: &str = include_str!("data/q10.data");
+
+fn process_data_a(data: &str) -> usize {
+    let mut rv = 0;
+    'line: for line in data.lines() {
+        let mut stack = vec![];
+        // Do something
+        for character in line.chars() {
+            match character {
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '{' => stack.push('}'),
+                '<' => stack.push('>'),
+
+                ')' => {
+                    if stack.pop() != Some(character) {
+                        rv += 3;
+                        continue 'line;
+                    }
+                }
+                ']' => {
+                    if stack.pop() != Some(character) {
+                        rv += 57;
+                        continue 'line;
+                    }
+                }
+                '}' => {
+                    if stack.pop() != Some(character) {
+                        rv += 1197;
+                        continue 'line;
+                    }
+                }
+                '>' => {
+                    if stack.pop() != Some(character) {
+                        rv += 25137;
+                        continue 'line;
+                    }
+                }
+                _ => {
+                    println!("Unknown character: {}", character);
+                }
+            }
+        }
+    }
+    rv
+}
+
+fn process_data_b(data: &str) -> usize {
+    let mut rv = vec![];
+    'line: for line in data.lines() {
+        let mut stack = vec![];
+        let mut line_rv = 0;
+        // Do something
+        for character in line.chars() {
+            match character {
+                '(' => stack.push(')'),
+                '[' => stack.push(']'),
+                '{' => stack.push('}'),
+                '<' => stack.push('>'),
+
+                ')' | ']' | '}' | '>' => {
+                    if stack.pop() != Some(character) {
+                        continue 'line;
+                    }
+                }
+                _ => {
+                    println!("Unknown character: {}", character);
+                }
+            }
+        }
+        for character in stack.into_iter().rev() {
+            line_rv *= 5;
+            match character {
+                ')' => {
+                    line_rv += 1;
+                }
+                ']' => {
+                    line_rv += 2;
+                }
+                '}' => {
+                    line_rv += 3;
+                }
+                '>' => {
+                    line_rv += 4;
+                }
+                _ => {
+                    println!("Unknown character: {}", character);
+                }
+            }
+        }
+        rv.push(line_rv);
+    }
+    rv.sort_unstable();
+    rv[rv.len() / 2]
+}
+//-----------------------------------------------------
+// Questions.
+
+q_impl!("10");
+
+#[test]
+fn a() {
+    assert_eq!(
+        process_data_a(indoc!(
+            "[({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
+        "
+        )),
+        26397
+    );
+}
+
+#[test]
+fn b() {
+    assert_eq!(
+        process_data_b(indoc!(
+            "[({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
+        "
+        )),
+        288957
+    );
+}
