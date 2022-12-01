faf71262a6331581c8c677d6aa92f3cc39334abc
Blake Winton <bwinton@latte.ca>
2022-12-01 07:20:56 -0500

    Finish day 1!

--- src/2022/q01.rs
+++ src/2022/q01.rs
@@ -4,21 +4,35 @@
 static INPUT: &str = include_str!("data/q01.data");
 
 fn process_data_a(data: &str) -> usize {
-    let mut rv = 0;
+    let mut elf = 0;
+    let mut calories = vec![];
     for line in data.lines() {
-        // Do something
-        rv += line.len() * 0;
+        if line.trim().is_empty() {
+            calories.push(elf);
+            elf = 0;
+            continue;
+        }
+        elf += line.parse::<usize>().unwrap();
     }
-    rv
+    calories.push(elf);
+    calories.into_iter().max().unwrap_or_default()
 }
 
 fn process_data_b(data: &str) -> usize {
-    let mut rv = 0;
+    let mut elf = 0;
+    let mut calories = vec![];
     for line in data.lines() {
-        // Do something
-        rv += line.len() * 0;
+        if line.trim().is_empty() {
+            calories.push(elf);
+            elf = 0;
+            continue;
+        }
+        elf += line.parse::<usize>().unwrap();
     }
-    rv
+    calories.push(elf);
+    calories.sort();
+    calories.reverse();
+    calories.into_iter().take(3).sum()
 }
 
 //-----------------------------------------------------
@@ -28,10 +42,48 @@ q_impl!("1");
 
 #[test]
 fn a() {
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(
+        process_data_a(indoc!(
+            "1000
+    2000
+    3000
+    
+    4000
+    
+    5000
+    6000
+    
+    7000
+    8000
+    9000
+    
+    10000
+    "
+        )),
+        24000
+    );
 }
 
 #[test]
 fn b() {
-    assert_eq!(process_data_b(indoc!("")), 0);
+    assert_eq!(
+        process_data_b(indoc!(
+            "1000
+    2000
+    3000
+    
+    4000
+    
+    5000
+    6000
+    
+    7000
+    8000
+    9000
+    
+    10000
+    "
+        )),
+        45000
+    );
 }
