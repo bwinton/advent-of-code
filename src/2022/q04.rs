fe6a0d2dc005d1d394f6369539edff513ddde9a2
Blake Winton <bwinton@latte.ca>
2022-12-04 00:25:08 -0500

    Finish day 4!

--- src/2022/q04.rs
+++ src/2022/q04.rs
@@ -3,11 +3,26 @@
 
 static INPUT: &str = include_str!("data/q04.data");
 
+fn get_segment(elves: &mut std::str::Split<char>) -> Vec<usize> {
+    elves
+        .next()
+        .unwrap()
+        .splitn(2, '-')
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<usize>>()
+}
+
 fn process_data_a(data: &str) -> usize {
     let mut rv = 0;
     for line in data.lines() {
-        // Do something
-        rv += line.len() * 0;
+        let mut elves = line.split(',');
+        let first = get_segment(&mut elves);
+        let second = get_segment(&mut elves);
+        if first[0] >= second[0] && first[1] <= second[1]
+            || second[0] >= first[0] && second[1] <= first[1]
+        {
+            rv += 1;
+        }
     }
     rv
 }
@@ -15,8 +30,14 @@ fn process_data_a(data: &str) -> usize {
 fn process_data_b(data: &str) -> usize {
     let mut rv = 0;
     for line in data.lines() {
-        // Do something
-        rv += line.len() * 0;
+        let mut elves = line.split(',');
+        let first = get_segment(&mut elves);
+        let second = get_segment(&mut elves);
+        if first[0] <= second[0] && first[1] >= second[0]
+            || first[0] >= second[0] && first[0] <= second[1]
+        {
+            rv += 1;
+        }
     }
     rv
 }
@@ -28,10 +49,32 @@ q_impl!("4");
 
 #[test]
 fn a() {
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(
+        process_data_a(indoc!(
+            "2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+    "
+        )),
+        2
+    );
 }
 
 #[test]
 fn b() {
-    assert_eq!(process_data_b(indoc!("")), 0);
+    assert_eq!(
+        process_data_b(indoc!(
+            "2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+    "
+        )),
+        4
+    );
 }
