2764d46a2e3859c32e3a18bc487f802139749228
Blake Winton <bwinton@latte.ca>
2022-12-06 07:00:04 -0500

    Cargo fmt.

--- src/2022/q06.rs
+++ src/2022/q06.rs
@@ -5,7 +5,7 @@ use std::collections::HashSet;
 
 static INPUT: &str = include_str!("data/q06.data");
 
-fn process(data: &str, size: usize) -> usize{
+fn process(data: &str, size: usize) -> usize {
     let mut rv = 0;
 
     let data: Vec<char> = data.chars().collect();
@@ -18,7 +18,6 @@ fn process(data: &str, size: usize) -> usize{
     }
     rv
 }
-
 
 fn process_data_a(data: &str) -> usize {
     process(data, 4)
@@ -38,8 +37,14 @@ fn a() {
     assert_eq!(process_data_a(indoc!("mjqjpqmgbljsphdztnvjfqwrcgsmlb")), 7);
     assert_eq!(process_data_a(indoc!("bvwbjplbgvbhsrlpgdmjqwftvncz")), 5);
     assert_eq!(process_data_a(indoc!("nppdvjthqldpwncqszvftbrmjlhg")), 6);
-    assert_eq!(process_data_a(indoc!("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")), 10);
-    assert_eq!(process_data_a(indoc!("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")), 11);
+    assert_eq!(
+        process_data_a(indoc!("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")),
+        10
+    );
+    assert_eq!(
+        process_data_a(indoc!("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")),
+        11
+    );
 }
 
 #[test]
@@ -47,6 +52,12 @@ fn b() {
     assert_eq!(process_data_b(indoc!("mjqjpqmgbljsphdztnvjfqwrcgsmlb")), 19);
     assert_eq!(process_data_b(indoc!("bvwbjplbgvbhsrlpgdmjqwftvncz")), 23);
     assert_eq!(process_data_b(indoc!("nppdvjthqldpwncqszvftbrmjlhg")), 23);
-    assert_eq!(process_data_b(indoc!("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")), 29);
-    assert_eq!(process_data_b(indoc!("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")), 26);
+    assert_eq!(
+        process_data_b(indoc!("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")),
+        29
+    );
+    assert_eq!(
+        process_data_b(indoc!("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")),
+        26
+    );
 }
