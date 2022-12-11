37a931a1b73fb42d5347362be5955de061c70343
Blake Winton <bwinton@latte.ca>
2022-12-11 02:07:55 -0500

    Fix the template to stop generating Clippy errors.

--- template/qXX.rs
+++ template/qXX.rs
@@ -4,19 +4,17 @@
 static INPUT: &str = include_str!("data/qXX.data");
 
 fn process_data_a(data: &str) -> usize {
-    let mut rv = 0;
-    for line in data.lines() {
+    let rv = 0;
+    for _line in data.lines() {
         // Do something
-        rv += line.len() * 0;
     }
     rv
 }
 
 fn process_data_b(data: &str) -> usize {
-    let mut rv = 0;
-    for line in data.lines() {
+    let rv = 0;
+    for _line in data.lines() {
         // Do something
-        rv += line.len() * 0;
     }
     rv
 }
