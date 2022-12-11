71daede5089b2954c1eb95ce6f439ad17a2ac3cc
Blake Winton <bwinton@latte.ca>
2022-12-11 02:07:36 -0500

    Set up day 12!

---
+++ src/2022/q12.rs
@@ -0,0 +1,35 @@
+//-----------------------------------------------------
+// Setup.
+
+static INPUT: &str = include_str!("data/q12.data");
+
+fn process_data_a(data: &str) -> usize {
+    let rv = 0;
+    for _line in data.lines() {
+        // Do something
+    }
+    rv
+}
+
+fn process_data_b(data: &str) -> usize {
+    let rv = 0;
+    for _line in data.lines() {
+        // Do something
+    }
+    rv
+}
+
+//-----------------------------------------------------
+// Questions.
+
+q_impl!("12");
+
+#[test]
+fn a() {
+    assert_eq!(process_data_a(indoc!("")), 0);
+}
+
+#[test]
+fn b() {
+    assert_eq!(process_data_b(indoc!("")), 0);
+}
