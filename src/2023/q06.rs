0b7b71b21d5e9b267a3203387589d3a683d5c2e0
Blake Winton <bwinton@latte.ca>
2023-12-06 09:47:17 -0500

    Finish day 6!

--- src/2023/q06.rs
+++ src/2023/q06.rs
@@ -1,22 +1,80 @@
 //-----------------------------------------------------
 // Setup.
 
+use itertools::Itertools;
+
 static INPUT: &str = include_str!("data/q06.data");
 
 fn process_data_a(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+    let mut rv = 1;
+    let mut lines = data.lines();
+    let times = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Time: ")
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let distances = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Distance: ")
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let races = times
+        .into_iter()
+        .zip(distances)
+        .collect::<Vec<(usize, usize)>>();
+    for (time, target) in races {
+        let mut wins = 0;
+        for i in 0..=time / 2 {
+            if i * (time - i) > target {
+                if i != time - i {
+                    wins += 2;
+                } else {
+                    wins += 1;
+                }
+            }
+        }
+        rv *= wins;
     }
     rv
 }
 
 fn process_data_b(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+    let mut lines = data.lines();
+    let time = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Time: ")
+        .unwrap()
+        .split_ascii_whitespace()
+        .join("")
+        .parse::<usize>()
+        .unwrap();
+    let target = lines
+        .next()
+        .unwrap()
+        .strip_prefix("Distance: ")
+        .unwrap()
+        .split_ascii_whitespace()
+        .join("")
+        .parse::<usize>()
+        .unwrap();
+    let mut wins = 0;
+    for i in 0..=time / 2 {
+        if i * (time - i) > target {
+            if i != time - i {
+                wins += 2;
+            } else {
+                wins += 1;
+            }
+        }
     }
-    rv
+    wins
 }
 
 //-----------------------------------------------------
@@ -28,12 +86,26 @@ q_impl!("6");
 fn a() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(
+        process_data_a(indoc!(
+            "Time:      7  15   30
+    Distance:  9  40  200
+    "
+        )),
+        288
+    );
 }
 
 #[test]
 fn b() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_b(indoc!("")), 0);
+    assert_eq!(
+        process_data_b(indoc!(
+            "Time:      7  15   30
+    Distance:  9  40  200
+    "
+        )),
+        71503
+    );
 }
