4f64b7fca601e9982c76989fc5b2260af1449ed9
Blake Winton <bwinton@latte.ca>
2023-12-07 07:09:52 -0500

    Set up day 8!

--- src/2023/main.rs
+++ src/2023/main.rs
@@ -6,6 +6,7 @@ mod q04;
 mod q05;
 mod q06;
 mod q07;
+mod q08;
 
 #[macro_use]
 extern crate aoc;
@@ -21,7 +22,7 @@ extern crate indoc;
 extern crate itertools;
 
 fn main() {
-    let days = q_vec!(q01, q02, q03, q04, q05, q06, q07,);
+    let days = q_vec!(q01, q02, q03, q04, q05, q06, q07, q08,);
 
     aoc::main(&days)
 }
