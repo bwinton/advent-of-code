67c42d094a44e054b784595a6e98a89ad546ca24
Blake Winton <bwinton@latte.ca>
2023-12-14 12:15:26 -0500

    Finish day 14.

--- src/2023/q14.rs
+++ src/2023/q14.rs
@@ -1,20 +1,151 @@
 //-----------------------------------------------------
 // Setup.
 
+use std::collections::HashMap;
+
+use aoc::util::Point2;
+
 static INPUT: &str = include_str!("data/q14.data");
 
-fn process_data_a(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+#[derive(Copy, Clone, Debug, PartialOrd, Ord, Hash, Eq, PartialEq)]
+enum Rock {
+    Rounded,
+    Cube,
+}
+
+#[allow(dead_code)]
+fn print_map(map: &HashMap<Point2, Rock>, max: Point2) -> String {
+    let mut rv = String::new();
+    for y in 0..=max.1 {
+        for x in 0..=max.0 {
+            rv.push(match map.get(&(x, y)) {
+                None => '.',
+                Some(Rock::Rounded) => 'O',
+                Some(Rock::Cube) => '#',
+            });
+        }
+        rv.push('\n');
     }
     rv
 }
 
+fn parse(data: &str) -> (HashMap<Point2, Rock>, Point2) {
+    let mut map = HashMap::new();
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    for (y, line) in data.lines().enumerate() {
+        for (x, cell) in line.chars().enumerate() {
+            max_x = max_x.max(x);
+            match cell {
+                '#' => {
+                    map.insert((x, y), Rock::Cube);
+                }
+                'O' => {
+                    map.insert((x, y), Rock::Rounded);
+                }
+                '.' => {}
+                _ => {
+                    panic!("Unknown character! {}", cell)
+                }
+            }
+        }
+        max_y = max_y.max(y);
+    }
+
+    (map, (max_x, max_y))
+}
+
+fn move_rock(
+    map: &mut HashMap<(usize, usize), Rock>,
+    curr: Point2,
+    max: Point2,
+    direction: (i32, i32),
+) {
+    if let Some(Rock::Rounded) = map.get(&curr) {
+        let mut new = (curr.0 as i32, curr.1 as i32);
+        // Move this as far as as possible.
+        loop {
+            new.0 += direction.0;
+            new.1 += direction.1;
+            if new.0 < 0 || new.1 < 0 || new.0 > max.0 as i32 || new.1 > max.1 as i32 {
+                map.remove(&curr);
+                new.0 -= direction.0;
+                new.1 -= direction.1;
+                map.insert((new.0 as usize, new.1 as usize), Rock::Rounded);
+                break;
+            }
+
+            if map.get(&(new.0 as usize, new.1 as usize)).is_some() {
+                // hit a rock;
+                map.remove(&curr);
+                new.0 -= direction.0;
+                new.1 -= direction.1;
+                map.insert((new.0 as usize, new.1 as usize), Rock::Rounded);
+                break;
+            }
+        }
+    }
+}
+
+fn process_data_a(data: &str) -> usize {
+    let mut rv = 0;
+    let (mut map, max) = parse(data);
+    for y in 0..=max.1 {
+        for x in 0..=max.0 {
+            move_rock(&mut map, (x, y), max, (0, -1));
+        }
+    }
+    for ((_, y), rock) in map {
+        if rock == Rock::Rounded {
+            rv += max.1 - y + 1;
+        }
+    }
+
+    rv
+}
+
 fn process_data_b(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+    let mut rv = 0;
+    let (mut map, max) = parse(data);
+    let mut seen = HashMap::new();
+    const CYCLE_COUNT: usize = 1000000000;
+    for cycle in 0..=CYCLE_COUNT {
+        let key = map.keys().cloned().collect::<Vec<_>>();
+        if let Some((i, cycle_sum)) = seen.get(&key) {
+            if (CYCLE_COUNT - cycle) % (cycle - i) == 0 {
+                rv = *cycle_sum;
+                break;
+            }
+        } else {
+            let mut cycle_sum = 0;
+            for ((_, y), rock) in &map {
+                if rock == &Rock::Rounded {
+                    cycle_sum += max.1 - y + 1;
+                }
+            }
+            seen.insert(key, (cycle, cycle_sum));
+        }
+        for y in 0..=max.1 {
+            for x in 0..=max.0 {
+                move_rock(&mut map, (x, y), max, (0, -1));
+            }
+        }
+        for x in 0..=max.0 {
+            for y in 0..=max.1 {
+                move_rock(&mut map, (x, y), max, (-1, 0));
+            }
+        }
+        for y in 0..=max.1 {
+            for x in 0..=max.0 {
+                move_rock(&mut map, (x, max.1 - y), max, (0, 1));
+            }
+        }
+        for x in 0..=max.0 {
+            for y in 0..=max.1 {
+                move_rock(&mut map, (max.0 - x, y), max, (1, 0));
+            }
+        }
     }
     rv
 }
@@ -28,12 +159,44 @@ q_impl!("14");
 fn a() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(
+        process_data_a(indoc!(
+            "
+    O....#....
+    O.OO#....#
+    .....##...
+    OO.#O....O
+    .O.....O#.
+    O.#..O.#.#
+    ..O..#O..O
+    .......O..
+    #....###..
+    #OO..#....
+    "
+        )),
+        136
+    );
 }
 
 #[test]
 fn b() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_b(indoc!("")), 0);
+    assert_eq!(
+        process_data_b(indoc!(
+            "
+    O....#....
+    O.OO#....#
+    .....##...
+    OO.#O....O
+    .O.....O#.
+    O.#..O.#.#
+    ..O..#O..O
+    .......O..
+    #....###..
+    #OO..#....
+    "
+        )),
+        64
+    );
 }
