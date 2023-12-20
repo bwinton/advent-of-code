f6718aa6b7443d7b2bed9809740f3f2dd166af5a
Blake Winton <bwinton@latte.ca>
2023-12-20 17:08:12 -0500

    Turns out it's faster to re-run the regex on each character of the string, going backwards. Like, 5.2 ms down to 3.5 ms…

--- src/2023/q01.rs
+++ src/2023/q01.rs
@@ -21,10 +21,7 @@ fn process_data_a(data: &str) -> u32 {
 
 fn process_data_b(data: &str) -> u32 {
     let mut rv = 0;
-    let digits = r"one|two|three|four|five|six|seven|eight|nine";
-    let forward_re = Regex::new(&format!("({}|[0-9])", digits)).unwrap();
-    let digits = digits.chars().rev().collect::<String>();
-    let backward_re = Regex::new(&format!("({}|[0-9])", digits)).unwrap();
+    let forward_re = Regex::new("(one|two|three|four|five|six|seven|eight|nine|[0-9])").unwrap();
     let map = HashMap::from([
         ("one", 1),
         ("two", 2),
@@ -35,38 +32,30 @@ fn process_data_b(data: &str) -> u32 {
         ("seven", 7),
         ("eight", 8),
         ("nine", 9),
-        ("0", 0),
-        ("1", 1),
-        ("2", 2),
-        ("3", 3),
-        ("4", 4),
-        ("5", 5),
-        ("6", 6),
-        ("7", 7),
-        ("8", 8),
-        ("9", 9),
-        ("eno", 1),
-        ("owt", 2),
-        ("eerht", 3),
-        ("ruof", 4),
-        ("evif", 5),
-        ("xis", 6),
-        ("neves", 7),
-        ("thgie", 8),
-        ("enin", 9),
     ]);
 
     for line in data.lines() {
         let first = forward_re
             .find_iter(line)
-            .filter_map(|x| map.get(x.as_str()))
+            .map(|x| {
+                map.get(x.as_str())
+                    .copied()
+                    .unwrap_or_else(|| x.as_str().parse().unwrap())
+            })
             .next()
             .unwrap();
-        let last = backward_re
-            .find_iter(&line.chars().rev().collect::<String>())
-            .filter_map(|x| map.get(x.as_str()))
-            .next()
-            .unwrap();
+        let mut last = 0;
+        let len = line.len();
+        for i in 1..=len {
+            if let Some(result) = forward_re.find_iter(&line[len - i..]).next() {
+                let result = map
+                    .get(result.as_str())
+                    .copied()
+                    .unwrap_or_else(|| result.as_str().parse().unwrap());
+                last = result;
+                break;
+            }
+        }
 
         rv += first * 10 + last;
     }
