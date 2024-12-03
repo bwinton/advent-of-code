22a8054625f0a554abbba7874a98daf76db105d3
Blake Winton <bwinton@latte.ca>
2024-12-03 06:56:59 -0500

    Finish day 3!

--- src/2024/q03.rs
+++ src/2024/q03.rs
@@ -1,20 +1,46 @@
+use regex::Regex;
+
 //-----------------------------------------------------
 // Setup.
 
 static INPUT: &str = include_str!("data/q03.data");
 
-fn process_data_a(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+fn process_data_a(data: &str) -> i32 {
+    let mut rv = 0;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    for line in data.lines() {
+        for c in re.captures_iter(line) {
+            let (full, [first, second]) = c.extract();
+            println!("match: {:?}, {}, {}", full, first, second);
+            let first: i32 = first.parse().unwrap();
+            let second: i32 = second.parse().unwrap();
+            rv += first * second;
+        }
     }
     rv
 }
 
-fn process_data_b(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+fn process_data_b(data: &str) -> i32 {
+    let mut rv = 0;
+    let re = Regex::new(r"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut add = true;
+    for line in data.lines() {
+        for c in re.captures_iter(line) {
+            let full = c.get(0).unwrap().as_str();
+            println!("match: {}, {}", full, &full[0..4]);
+            match &full[0..4] {
+                "do()" => add = true,
+                "don'" => add = false,
+                "mul(" => {
+                    if add {
+                        let first: i32 = c.get(1).unwrap().as_str().parse().unwrap();
+                        let second: i32 = c.get(2).unwrap().as_str().parse().unwrap();
+                        rv += first * second;
+                    }
+                }
+                x => panic!("Don't understand {}", x),
+            }
+        }
     }
     rv
 }
@@ -28,12 +54,22 @@ q_impl!("3");
 fn a() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(
+        process_data_a(indoc!(
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        )),
+        161
+    );
 }
 
 #[test]
 fn b() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_b(indoc!("")), 0);
+    assert_eq!(
+        process_data_b(indoc!(
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        )),
+        48
+    );
 }
