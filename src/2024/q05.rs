05d42dd23b404b2adde1afaf2973652e1403f841
Blake Winton <bwinton@latte.ca>
2024-12-05 00:02:26 -0500

    Finish day 5!

--- src/2024/q05.rs
+++ src/2024/q05.rs
@@ -1,20 +1,80 @@
 //-----------------------------------------------------
 // Setup.
 
+use nom::{
+    IResult,
+    bytes::complete::tag,
+    character::complete::{newline, u32},
+    multi::{many1, separated_list1},
+    sequence::tuple,
+};
+
+type Orderings = Vec<(u32, u32)>;
+type Updates = Vec<Vec<u32>>;
+
 static INPUT: &str = include_str!("data/q05.data");
 
-fn process_data_a(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+fn page_ordering(i: &str) -> IResult<&str, (u32, u32)> {
+    let (input, (first, _, second, _)) = tuple((u32, tag("|"), u32, newline))(i)?;
+    Ok((input, (first, second)))
+}
+
+fn page_orderings(i: &str) -> IResult<&str, Orderings> {
+    let (input, data) = many1(page_ordering)(i)?;
+    Ok((input, data))
+}
+
+fn update(i: &str) -> IResult<&str, Vec<u32>> {
+    let (input, (data, _)) = tuple((separated_list1(tag(","), u32), newline))(i)?;
+    Ok((input, data))
+}
+
+fn updates(i: &str) -> IResult<&str, Updates> {
+    let (input, data) = many1(update)(i)?;
+    Ok((input, data))
+}
+
+fn parser(i: &str) -> IResult<&str, (Orderings, Updates)> {
+    let (input, (order, _, updates)) = tuple((page_orderings, newline, updates))(i)?;
+    Ok((input, (order, updates)))
+}
+
+fn process_data_a(data: &str) -> u32 {
+    let mut rv = 0;
+    let (order, updates) = parser(data).unwrap().1;
+    for update in updates {
+        let mut valid = true;
+        'outer: for i in 0..update.len() {
+            for j in i + 1..update.len() {
+                if !order.contains(&(update[i], update[j])) {
+                    valid = false;
+                    break 'outer;
+                }
+            }
+        }
+        if valid {
+            rv += update[update.len() / 2];
+        }
     }
     rv
 }
 
-fn process_data_b(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+fn process_data_b(data: &str) -> u32 {
+    let mut rv = 0;
+    let (order, mut updates) = parser(data).unwrap().1;
+    for update in updates.iter_mut() {
+        let mut valid = true;
+        for i in 0..update.len() {
+            for j in i + 1..update.len() {
+                if !order.contains(&(update[i], update[j])) {
+                    valid = false;
+                    update.swap(i, j);
+                }
+            }
+        }
+        if !valid {
+            rv += update[update.len() / 2];
+        }
     }
     rv
 }
@@ -28,12 +88,78 @@ q_impl!("5");
 fn a() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(
+        process_data_a(indoc!(
+            "47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+            "
+        )),
+        143
+    );
 }
 
 #[test]
 fn b() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_b(indoc!("")), 0);
+    assert_eq!(
+        process_data_b(indoc!(
+            "47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+            "
+        )),
+        123
+    );
 }
