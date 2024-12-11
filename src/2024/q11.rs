9238e46abe37bfcf1cc0e9533714cc7b874d45df
Blake Winton <bwinton@latte.ca>
2024-12-11 08:47:48 -0500

    Day 11 part 2.

--- src/2024/q11.rs
+++ src/2024/q11.rs
@@ -5,70 +5,57 @@ use std::collections::HashMap;
 
 static INPUT: &str = include_str!("data/q11.data");
 
-fn blink(stones: &Vec<String>, seen: &HashMap<String, Vec<String>>) -> Vec<String> {
-    let mut stones = stones.clone();
-    let mut rv = vec![];
-    let check = stones.join(" ");
-    println!("Looking in {}", check);
-    for key in seen.keys() {
-        if check.contains(key) {
-            println!("  Found key!!! {} in {}", check.len(), key.len());
+fn stone_count(stone: String, iterations: i32, cache: &mut HashMap<(String, i32), usize>) -> usize {
+    if iterations == 0 {
+        return 1;
+    };
+
+    if let Some(&rv) = cache.get(&(stone.clone(), iterations)) {
+        return rv;
+    };
+
+    let mut rv;
+    if stone == *"0" {
+        rv = stone_count("1".to_owned(), iterations - 1, cache);
+    } else if stone.len() % 2 == 0 {
+        let clone = stone.clone();
+        let (left, right) = clone.split_at(stone.len() / 2);
+        rv = stone_count(left.to_owned(), iterations - 1, cache);
+        let mut right = right.trim_start_matches('0').to_owned();
+        if right.is_empty() {
+            right = "0".to_owned();
         }
+        rv += stone_count(right, iterations - 1, cache);
+    } else {
+        rv = stone_count(
+            format!("{}", stone.parse::<i64>().unwrap() * 2024),
+            iterations - 1,
+            cache,
+        );
     }
-    stones.reverse();
-    while !stones.is_empty() {
-        let stone = stones.pop().unwrap();
-        // If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-        // print!("  {}: {} => ", i, stone);
-        if stone == "0".to_owned() {
-            rv.push("1".to_owned());
-        } else if stone.len() % 2 == 0 {
-            // If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-            let clone = stone.clone();
-            let (left, right) = clone.split_at(stone.len() / 2);
-            // println!("  l:{:?}, r:{:?}", left, right);
-            rv.push(left.to_owned());
-            let mut right = right.trim_start_matches('0').to_owned();
-            // println!("r0: {:?}, {}", right, right.is_empty());
-            if right.is_empty() {
-                // println!("r1: {:?}", right);
-                right = "0".to_owned();
-            }
-            // println!("r2: {:?}", right);
-            rv.push(right);
-        } else {
-            // If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
-            rv.push(format!("{}", stone.parse::<i64>().unwrap() * 2024));
-        }
-        // println!("{}", stone);
-    }
+    cache.insert((stone, iterations), rv);
     rv
 }
 
 fn process_data_a(data: &str) -> usize {
-    let mut stones: Vec<String> = data.split_whitespace().map(|s| s.to_owned()).collect();
-    let mut seen: HashMap<String, Vec<String>> = HashMap::new();
-    for _i in 0..25 {
-        println!("{:02}: {:?}", _i, stones.len());
-        let next = blink(&stones, &seen);
-        seen.insert(stones.join(" "), next.clone());
-        stones = next;
+    let stones: Vec<String> = data.split_whitespace().map(|s| s.to_owned()).collect();
+    let mut rv = 0;
+    let mut cache = HashMap::new();
+    for stone in stones {
+        rv += stone_count(stone, 25, &mut cache);
     }
-    // println!("25: {:?}", stones);
-    stones.len()
+    rv
 }
 
 fn process_data_b(data: &str) -> usize {
-    let mut stones: Vec<String> = data.split_whitespace().map(|s| s.to_owned()).collect();
-    let mut seen: HashMap<String, Vec<String>> = HashMap::new();
-    for _i in 0..75 {
-        println!("{:02}: {}", _i, stones.len());
-        let next = blink(&stones, &seen);
-        seen.insert(stones.join(" "), next.clone());
-        stones = next;
+    let stones: Vec<String> = data.split_whitespace().map(|s| s.to_owned()).collect();
+    let mut rv = 0;
+    let mut cache = HashMap::new();
+    for stone in stones {
+        let add = stone_count(stone, 75, &mut cache);
+        rv += add;
     }
-    // println!("25: {:?}", stones);
-    stones.len()
+    rv
 }
 
 //-----------------------------------------------------
@@ -79,43 +66,6 @@ q_impl!("11");
 #[test]
 fn a() {
     use pretty_assertions::assert_eq;
-
-    // let data: Vec<String> = "0 1 10 99 999".split_whitespace().map(|s| s.to_owned()).collect();
-    // let expected: Vec<String> = "1 2024 1 0 9 9 2021976".split_whitespace().map(|s| s.to_owned()).collect();
-    // let data = blink(&data, &HashMap::new());
-    // assert_eq!(data, expected);
-
-    // let data: Vec<String> = "125 17".split_whitespace().map(|s| s.to_owned()).collect();
-    // let expected: Vec<String> = "253000 1 7".split_whitespace().map(|s| s.to_owned()).collect();
-    // let data = blink(&data, &HashMap::new());
-    // // println!("data: {:?}", data);
-    // assert_eq!(data, expected);
-
-    // let expected: Vec<String> = "253 0 2024 14168".split_whitespace().map(|s| s.to_owned()).collect();
-    // let data = blink(&data, &HashMap::new());
-    // // println!("data: {:?}", data);
-    // assert_eq!(data, expected);
-
-    // let expected: Vec<String> = "512072 1 20 24 28676032".split_whitespace().map(|s| s.to_owned()).collect();
-    // let data = blink(&data, &HashMap::new());
-    // // println!("data: {:?}", data);
-    // assert_eq!(data, expected);
-
-    // let expected: Vec<String> = "512 72 2024 2 0 2 4 2867 6032".split_whitespace().map(|s| s.to_owned()).collect();
-    // let data = blink(&data, &HashMap::new());
-    // // println!("data: {:?}", data);
-    // assert_eq!(data, expected);
-
-    // let expected: Vec<String> = "1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32".split_whitespace().map(|s| s.to_owned()).collect();
-    // let data = blink(&data, &HashMap::new());
-    // // println!("data: {:?}", data);
-    // assert_eq!(data, expected);
-
-
-    // let expected: Vec<String> = "2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2".split_whitespace().map(|s| s.to_owned()).collect();
-    // let data = blink(&data, &HashMap::new());
-    // // println!("data: {:?}", data);
-    // assert_eq!(data, expected);
 
     assert_eq!(process_data_a(indoc!("125 17")), 55312);
 }
