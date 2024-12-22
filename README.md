1c12e2e61d8351ae67d87f42d5a32e8ecad2bb85
Blake Winton <bwinton@latte.ca>
2024-12-22 11:52:28 -0500

    Finish day 22!

--- README.md
+++ README.md
@@ -762,5 +762,5 @@
 
 ## Timings
 
-timings from `echo "" > timings.out;for i in {1..21}
+timings from `echo "" > timings.out;for i in {1..22}
   hyperfine -N --warmup 10 "./target/release/2024 $i" >> timings.out`
--- src/2024/q22.rs
+++ src/2024/q22.rs
@@ -1,22 +1,85 @@
 //-----------------------------------------------------
 // Setup.
 
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+
 static INPUT: &str = include_str!("data/q22.data");
 
 fn process_data_a(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+    let mut monkeys: Vec<usize> = data.lines().map(|x| x.parse().unwrap()).collect();
+    // println!("monkeys: {:?}", monkeys);
+
+    for _i in 0..2000 {
+        for monkey in monkeys.iter_mut() {
+            // println!("{}", monkey);
+            let result = *monkey * 64;
+            *monkey ^= result;
+            *monkey %= 16777216;
+            let result = *monkey / 32;
+            *monkey ^= result;
+            *monkey %= 16777216;
+            let result = *monkey * 2048;
+            *monkey ^= result;
+            *monkey %= 16777216;
+        }
     }
-    rv
+    monkeys.iter().sum()
 }
 
 fn process_data_b(data: &str) -> usize {
-    let rv = 0;
-    for _line in data.lines() {
-        // Do something
+    let mut monkeys: Vec<usize> = data.lines().map(|x| x.parse().unwrap()).collect();
+    // println!("monkeys: {:?}", monkeys);
+
+    let mut prices = vec![vec![]; monkeys.len()];
+    for _i in 0..2000 {
+        for (i, monkey) in monkeys.iter_mut().enumerate() {
+            // println!("{}", monkey);
+            let result = *monkey * 64;
+            *monkey ^= result;
+            *monkey %= 16777216;
+            let result = *monkey / 32;
+            *monkey ^= result;
+            *monkey %= 16777216;
+            let result = *monkey * 2048;
+            *monkey ^= result;
+            *monkey %= 16777216;
+            prices[i].push(*monkey % 10);
+        }
     }
-    rv
+    let changes = prices
+        .par_iter()
+        .map(|x| {
+            let mut change = HashMap::new();
+            for (a, b, c, d, e) in x.iter().tuple_windows() {
+                let a = *a as i8;
+                let b = *b as i8;
+                let c = *c as i8;
+                let d = *d as i8;
+                let key = (b - a, c - b, d - c, *e as i8 - d);
+                change.entry(key).or_insert(e);
+            }
+            change
+        })
+        .collect::<Vec<_>>();
+
+    let mut grouped_changes: HashMap<&(i8, i8, i8, i8), usize> = HashMap::new();
+    for change in changes.iter() {
+        for (k, &&v) in change.iter() {
+            *grouped_changes.entry(k).or_default() += v;
+        }
+    }
+    let mut best_change = ((0, 0, 0, 0), 0);
+    for change in grouped_changes {
+        if change.1 > best_change.1 {
+            // println!("better change: {:?}", change);
+            best_change = (*change.0, change.1);
+        }
+    }
+    // println!("best_change: {:?}", best_change);
+    best_change.1
 }
 
 //-----------------------------------------------------
@@ -28,12 +91,43 @@ q_impl!("22");
 fn a() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_a(indoc!("")), 0);
+    assert_eq!(process_data_a(indoc!("1")), 8685429);
+    assert_eq!(process_data_a(indoc!("10")), 4700978);
+    assert_eq!(process_data_a(indoc!("100")), 15273692);
+    assert_eq!(process_data_a(indoc!("2024")), 8667524);
+
+    assert_eq!(
+        process_data_a(indoc!(
+            "
+        1
+        10
+        100
+        2024
+        "
+        )),
+        37327623
+    );
 }
 
 #[test]
 fn b() {
     use pretty_assertions::assert_eq;
 
-    assert_eq!(process_data_b(indoc!("")), 0);
+    // assert_eq!(process_data_b(indoc!("123")), 7);
+    // assert_eq!(process_data_b(indoc!("1")), 7);
+    // assert_eq!(process_data_b(indoc!("10")), 7);
+    // assert_eq!(process_data_b(indoc!("100")), 0);
+    // assert_eq!(process_data_b(indoc!("2024")), 9);
+
+    assert_eq!(
+        process_data_b(indoc!(
+            "
+        1
+        2
+        3
+        2024
+        "
+        )),
+        23
+    );
 }
