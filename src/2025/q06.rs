84b3b07d09a5675652ed94c501a86521dca2d40b
Blake Winton <bwinton@latte.ca>
2025-12-05 16:01:34 -0500

    Set up day 6.

---
+++ src/2025/q06.rs
@@ -0,0 +1,39 @@
+//-----------------------------------------------------
+// Setup.
+
+static INPUT: &str = include_str!("data/q06.data");
+
+fn process_data_a(data: &str) -> usize {
+    let rv = 0;
+    for _line in data.lines() {
+        // Do something
+    }
+    rv
+}
+
+fn process_data_b(data: &str) -> usize {
+    let rv = 0;
+    for _line in data.lines() {
+        // Do something
+    }
+    rv
+}
+
+//-----------------------------------------------------
+// Questions.
+
+q_impl!("6");
+
+#[test]
+fn a() {
+    use pretty_assertions::assert_eq;
+
+    assert_eq!(process_data_a(indoc!("")), 0);
+}
+
+#[test]
+fn b() {
+    use pretty_assertions::assert_eq;
+
+    assert_eq!(process_data_b(indoc!("")), 0);
+}
