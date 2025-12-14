18a7aa347feb9292b51c5b653f9216df3bf07a13
Blake Winton <bwinton@latte.ca>
2025-12-14 16:49:47 -0500

    Set up day 11 and 12.

--- src/2025/main.rs
+++ src/2025/main.rs
@@ -11,6 +11,8 @@ mod q07;
 mod q08;
 mod q09;
 mod q10;
+mod q11;
+mod q12;
 
 #[macro_use]
 extern crate aoc;
@@ -23,7 +25,7 @@ extern crate indoc;
 // extern crate itertools;
 
 fn main() {
-    let days = q_vec!(q01, q02, q03, q04, q05, q06, q07, q08, q09, q10,);
+    let days = q_vec!(q01, q02, q03, q04, q05, q06, q07, q08, q09, q10, q11, q12,);
 
     aoc::main(&days)
 }
